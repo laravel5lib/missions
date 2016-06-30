25c15ec623bf65030dbd5b596f99f6879b2db2df
Neil Keena <nkeena1@gmail.com>
2016-06-30 11:33:52 -0400

    updated depreciated method

--- app/Models/v1/Reservation.php
+++ app/Models/v1/Reservation.php
@@ -218,7 +218,7 @@ class Reservation extends Model
         if ( ! $costs instanceof Collection)
             $costs = collect($costs);
 
-        $data = $costs->lists('id')->toArray();
+        $data = $costs->pluck('id')->all();
         
         $this->costs()->sync($data);
     }

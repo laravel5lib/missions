705839735e467b3ff6d7542fc23df7bc50212ea9
Jerez Bain <jerezb31@gmail.com>
2016-07-11 15:28:33 -0400

    Added fundraisers page

--- resources/views/dashboard/reservations/layouts/menu.blade.php
+++ resources/views/dashboard/reservations/layouts/menu.blade.php
@@ -1,9 +1,10 @@
-<a href="{{ url('dashboard/reservations') }}" class="btn btn-block btn-primary"><span class="fa fa-chevron-left"></span> My Reservations</a>
+<a href="{{ url('dashboard/reservations') }}" class="btn btn-block btn-default"><span class="fa fa-chevron-left"></span> My Reservations</a>
+<hr>
 <ul class="nav nav-tabs nav-stacked">
     <li role="presentation" class="{{ $active == 'details' ? 'active' : '' }}"><a href="{{ url('dashboard/reservations', [$reservation->id]) }}">Details</a></li>
     <li role="presentation" class="{{ $active == 'requirements' ? 'active' : '' }}"><a href="{{ url('dashboard/reservations', [$reservation->id, 'requirements']) }}">Travel Documents</a></li>
     <li role="presentation" class="{{ $active == 'donations' ? 'active' : '' }}"><a href="{{ url('dashboard/reservations', [$reservation->id, 'donations']) }}">Donations</a></li>
-    <li role="presentation" class="{{ $active == 'funding' ? 'active' : '' }}"><a href="{{ url('dashboard/reservations', [$reservation->id, 'funding']) }}">Funding</a></li>
     <li role="presentation" class="{{ $active == 'fundraisers' ? 'active' : '' }}"><a href="{{ url('dashboard/reservations', [$reservation->id, 'fundraisers']) }}">Fundraisers</a></li>
+    <li role="presentation" class="{{ $active == 'funding' ? 'active' : '' }}"><a href="{{ url('dashboard/reservations', [$reservation->id, 'funding']) }}">Funding</a></li>
     <li role="presentation" class="{{ $active == 'deadlines' ? 'active' : '' }}"><a href="{{ url('dashboard/reservations', [$reservation->id, 'deadlines']) }}">Due Dates</a></li>
 </ul>

107eda5baac0bae42bcc25f5fc4c5183dc60df03
Neil Keena <nkeena1@gmail.com>
2016-07-29 00:52:26 -0400

    update language and dashboard toolbar

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -4,7 +4,7 @@
   <div class="container">
     <div class="row">
       <div class="col-sm-12">
-        <h1 class="text-muted text-center">User Dashboard</h1>
+        <h1 class="text-muted text-center">My Dashboard</h1>
       </div>
     </div>
   </div>

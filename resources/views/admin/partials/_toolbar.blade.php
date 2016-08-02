ff9d585bda9709ea4cdebbdd46d8da2ccdc55059
Jerez Bain <jerezb31@gmail.com>
2016-08-02 09:30:21 -0400

    Restore toolbars

--- resources/views/admin/partials/_toolbar.blade.php
+++ resources/views/admin/partials/_toolbar.blade.php
@@ -1,1 +1,9 @@
-<admin-sidebar></admin-sidebar>
+<div id="myNavmenu" class="navmenu navmenu-inverse navmenu-fixed-left offcanvas-sm">
+  <ul class="nav navmenu-nav">
+    <li ><a href="/admin"><i class="fa fa-tachometer"></i></a></li>
+    <li><a href="/admin/campaigns"><i class="fa fa-globe"></i></a></li>
+    <li><a href="/admin/reservations"><i class="fa fa-ticket"></i></a></li>
+    <li><a href="/admin/groups"><i class="fa fa-users"></i></a></li>
+    <li><a href="/admin/users"><i class="fa fa-user"></i></a></li>
+  </ul>
+</div>
--- resources/views/dashboard/partials/_toolbar.blade.php
+++ resources/views/dashboard/partials/_toolbar.blade.php
@@ -1,1 +1,7 @@
-<sidebar></sidebar>
+<div id="myNavmenu" class="navmenu navmenu-inverse navmenu-fixed-left offcanvas-sm">
+  <ul class="nav navmenu-nav">
+    <li ><a href="/dashboard"><i class="fa fa-tachometer"></i></a></li>
+    <li><a href="/dashboard/reservations"><i class="fa fa-ticket"></i></a></li>
+    <li><a href="/dashboard/records"><i class="fa fa-archive"></i></a></li>
+  </ul>
+</div>

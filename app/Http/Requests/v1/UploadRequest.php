0e9500090dfd189fca8ec81229d22c2ff8db2ee6
Neil Keena <nkeena1@gmail.com>
2016-08-05 16:29:21 -0400

    change tag names to capitalized

--- app/Http/Requests/v1/UploadRequest.php
+++ app/Http/Requests/v1/UploadRequest.php
@@ -34,7 +34,7 @@ class UploadRequest extends FormRequest
             'width'  => 'numeric',
             'height' => 'numeric',
             'tags'   => 'required|array',
-            'tags.*' => 'in:campaign,user,group,fundraiser'
+            'tags.*' => 'in:Campaign,User,Group,Fundraiser'
         ];
 
         if($this->method('put'))
@@ -50,7 +50,7 @@ class UploadRequest extends FormRequest
                 'width'  => 'numeric',
                 'height' => 'numeric',
                 'tags'   => 'required|array',
-                'tags.*' => 'in:campaign,user,group,fundraiser'
+                'tags.*' => 'in:Campaign,User,Group,Fundraiser'
             ];
         }
 

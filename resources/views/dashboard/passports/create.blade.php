517076f382d7b368076e5ac01540aa40c2a68e01
Jerez Bain <jerezb31@gmail.com>
2016-08-11 14:15:38 -0400

    jCrop dependencies added

--- resources/views/dashboard/passports/create.blade.php
+++ resources/views/dashboard/passports/create.blade.php
@@ -1,5 +1,12 @@
 @extends('dashboard.layouts.default')
 
+@section('styles')
+    <link rel="stylesheet" href="http://jcrop-cdn.tapmodo.com/v2.0.0-RC1/css/Jcrop.css" type="text/css">
+@endsection
+@section('scripts')
+    <script src="http://jcrop-cdn.tapmodo.com/v2.0.0-RC1/js/Jcrop.js"></script>
+@endsection
+
 @section('content')
     <div class="container">
         <div class="row">

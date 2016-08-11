8eac165cdd77bfca11fa9ffed291ec41548a6ee7
Jerez Bain <jerezb31@gmail.com>
2016-08-11 14:26:29 -0400

    add edit page

---
+++ resources/views/dashboard/passports/edit.blade.php
@@ -0,0 +1,26 @@
+@extends('dashboard.layouts.default')
+
+@section('styles')
+    <link rel="stylesheet" href="http://jcrop-cdn.tapmodo.com/v2.0.0-RC1/css/Jcrop.css" type="text/css">
+@endsection
+@section('scripts')
+    <script src="http://jcrop-cdn.tapmodo.com/v2.0.0-RC1/js/Jcrop.js"></script>
+@endsection
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <h1 class="text-muted text-center">My Passports <small>Edit</small></h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-8">
+                <passport-create-update></passport-create-update>
+            </div>
+            <div class="col-sm-4">
+                <img src="/images/passport-placeholder.png" alt="" style="width: 100%;height: auto;">
+            </div>
+        </div>
+    </div>
+@endsection

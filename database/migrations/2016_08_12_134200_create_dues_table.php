fc5af6aee6e88d2172d864fbd1296894e19d2ce7
Neil Keena <nkeena1@gmail.com>
2016-08-12 11:11:47 -0400

    add payment dues

---
+++ database/migrations/2016_08_12_134200_create_dues_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDuesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dues', function(Blueprint $table) {
+            $table->uuid('reservation_id')->index();
+            $table->uuid('payment_id')->index();
+            $table->integer('outstanding_balance')->default(0);
+            $table->dateTime('due_at');
+            $table->integer('grace_period');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('dues');
+    }
+}

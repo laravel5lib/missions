701ec3f282cb7afc547af56a77683cff9a710b0e
Neil Keena <nkeena1@gmail.com>
2016-08-17 16:50:11 -0400

    finish StoriesController and add StoryRequest classes

--- app/Http/Controllers/Api/StoriesController.php
+++ app/Http/Controllers/Api/StoriesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Requests\v1\StoryRequest;
 use App\Http\Transformers\v1\StoryTransformer;
 use App\Models\v1\Story;
 use Illuminate\Http\Request;
@@ -24,6 +25,12 @@ class StoriesController extends Controller
         $this->story = $story;
     }
 
+    /**
+     * Get a list of stories.
+     *
+     * @param Request $request
+     * @return \Dingo\Api\Http\Response
+     */
     public function index(Request $request)
     {
         $stories = $this->story
@@ -31,5 +38,62 @@ class StoriesController extends Controller
             ->paginate($request->get('per_page', 10));
 
         return $this->response->paginator($stories, new StoryTransformer);
+    }
+
+    /**
+     * Get the specified story.
+     *
+     * @param $id
+     * @return \Dingo\Api\Http\Response
+     */
+    public function show($id)
+    {
+        $story = $this->story->findOrFail($id);
+
+        return $this->response->item($story, new StoryTransformer);
+    }
+
+    /**
+     * Create a new story.
+     *
+     * @param StoryRequest $request
+     * @return \Dingo\Api\Http\Response
+     */
+    public function store(StoryRequest $request)
+    {
+        $story = $this->story->create($request->all());
+
+        return $this->response->item($story, new StoryTransformer);
+    }
+
+    /**
+     * Update the specified story.
+     *
+     * @param StoryRequest $request
+     * @param $id
+     * @return \Dingo\Api\Http\Response
+     */
+    public function update(StoryRequest $request, $id)
+    {
+        $story = $this->story->findOrFail($id);
+
+        $story->update($request->all());
+
+        return $this->response->item($story, new StoryTransformer);
+    }
+
+    /**
+     * Delete the specified story.
+     *
+     * @param $id
+     * @return \Dingo\Api\Http\Response
+     */
+    public function destroy($id)
+    {
+        $story = $this->story->findOrFail($id);
+
+        $story->delete();
+
+        return $this->response->noContent();
     }
 }
---
+++ app/Http/Requests/v1/StoryRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\v1;
+
+use App\Http\Requests\Request;
+
+class StoryRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required|string',
+            'content' => 'required|string',
+            'author_id' => 'required|string',
+            'author_type' => 'required|string|in:users,groups,fundraisers'
+        ];
+    }
+}

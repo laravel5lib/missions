42b67bfe20866d9cdd20576bfb037a132d231488
Jerez Bain <jerezb31@gmail.com>
2016-08-30 03:23:17 -0400

    Fundraisers donors and donations toggleable

--- resources/assets/js/components/users/user-profile-fundraisers-donors.vue
+++ resources/assets/js/components/users/user-profile-fundraisers-donors.vue
@@ -1,48 +1,54 @@
 <template>
     <div class="">
-        <div class="panel panel-default" v-for="donor in donors">
+        <div class="btn-group btn-group-xs btn-group-justified" role="group" aria-label="...">
+            <div class="btn-group btn-group-xs" role="group">
+                <button type="button" class="btn btn-default" :class="{'btn-primary': activeView === 'donors'}" @click="toggleView('donors')">Donors</button>
+            </div>
+            <div class="btn-group btn-group-xs" role="group">
+                <button type="button" class="btn btn-default" :class="{'btn-primary': activeView === 'donations'}" @click="toggleView('donations')">Donations</button>
+            </div>
+        </div>
+
+        <hr class="divider sm">
+
+        <div class="panel panel-default" v-for="donor in donors" v-if="activeView==='donors'">
             <div class="panel-heading" role="tab" id="heading-{{ donor.id }}">
                 <h5>
-                    <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse-{{ donor.id }}" aria-expanded="true" aria-controls="collapseOne">
+                    <a role="button">
                         {{ donor.name }} <span class="small text-success">{{donor.total_donated|currency}}</span>
                     </a>
                 </h5>
             </div>
-            <div id="collapse-{{ donor.id }}" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading-{{ donor.id }}">
-                <ul class="list-group">
-                    <li class="list-group-item" v-for="donation in donor.donations.data">
-                        <h5 class="list-group-item-heading"><a :href="'@' + donor.account_holder_id">{{ donor.name }}</a> <small>donated {{ donation.amount|currency }} on {{ donation.created_at|moment 'll'}}</small></h5>
-                        <p class="list-group-item-text" v-html="donation.message | marked"></p>
-                    </li>
-                </ul>
-                <!--<div class="panel-body"></div>-->
+        </div>
+        <div class="panel panel-default" v-for="donation in donations" v-if="activeView==='donations'">
+            <div class="panel-heading" role="tab" id="heading-{{ donation.id }}">
+                <h5>
+                    <a :href="'@' + donation.donor.data.account_url">{{ donation.name }}</a><br>
+                    <small class="small text-success">donated {{ donation.amount|currency }} on {{ donation.created_at|moment 'll'}}</small>
+                </h5>
             </div>
-            <!--<div class="panel-footer"></div>-->
         </div>
 
-                </tbody>
-            </table>
-            <div class="row">
-                <div class="col-sm-12 text-center">
-                    <nav>
-                        <ul class="pagination pagination-sm">
-                            <li>
-                                <a>{{ pagination.total }} Donors</a>
-                            </li>
-                            <li :class="{ 'disabled': pagination.current_page == 1 }">
-                                <a aria-label="Previous" @click="page=pagination.current_page-1">
-                                    <span aria-hidden="true">&laquo; Back</span>
-                                </a>
-                            </li>
-                            <!--<li :class="{ 'active': (n+1) == pagination.current_page}" v-for="n in pagination.total_pages"><a @click="page=(n+1)">{{(n+1)}}</a></li>-->
-                            <li :class="{ 'disabled': pagination.current_page == pagination.total_pages }">
-                                <a aria-label="Next" @click="page=pagination.current_page+1">
-                                    <span aria-hidden="true">Next &raquo;</span>
-                                </a>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <nav>
+                    <ul class="pagination pagination-sm">
+                        <li>
+                            <a>{{ pagination.total }} {{ activeView === 'donors' ? 'Donors' : 'Donations' }}</a>
+                        </li>
+                        <li :class="{ 'disabled': pagination.current_page == 1 }">
+                            <a aria-label="Previous" @click="page=pagination.current_page-1">
+                                <span aria-hidden="true">&laquo; Back</span>
+                            </a>
+                        </li>
+                        <!--<li :class="{ 'active': (n+1) == pagination.current_page}" v-for="n in pagination.total_pages"><a @click="page=(n+1)">{{(n+1)}}</a></li>-->
+                        <li :class="{ 'disabled': pagination.current_page == pagination.total_pages }">
+                            <a aria-label="Next" @click="page=pagination.current_page+1">
+                                <span aria-hidden="true">Next &raquo;</span>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
             </div>
         </div>
     </div>
@@ -55,6 +61,9 @@
         data(){
             return{
                 donors: [],
+                donations: [],
+                activeView: 'donors',
+
                 // pagination vars
                 page: 1,
                 per_page: 10,
@@ -70,9 +79,27 @@
             },
         },
         methods:{
+            toggleView(view){
+                switch (view) {
+                    case 'donors':
+                        this.searchDonors();
+                        break;
+                    case 'donations':
+                        this.searchDonations();
+                        break;
+                }
+                this.activeView = view;
+//                this.activeView = this.activeView === 'donors' ? 'donations' : 'donors';
+            },
             searchDonors(){
-                this.$http.get('fundraisers{/id}/donors', {id: this.id, include: 'donations'}).then(function (response) {
+                this.$http.get('fundraisers{/id}/donors', {id: this.id}).then(function (response) {
                     this.donors = response.data.data;
+                    this.pagination = response.data.meta.pagination;
+                });
+            },
+            searchDonations(){
+                this.$http.get('fundraisers{/id}/donations', {id: this.id, include: 'donor'}).then(function (response) {
+                    this.donations = response.data.data;
                     this.pagination = response.data.meta.pagination;
                 });
             }

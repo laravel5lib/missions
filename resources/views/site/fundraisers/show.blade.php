9440432496930be4a49de155ed423a7373b08c2c
Jerez Bain <jerezb31@gmail.com>
2016-09-06 11:57:04 -0400

    groups cards show group type + donation form updates

--- resources/views/site/fundraisers/show.blade.php
+++ resources/views/site/fundraisers/show.blade.php
@@ -52,7 +52,7 @@
                         <h6 class="text-center small text-muted"><i class="fa fa-calendar"></i> Deadline is {{ $fundraiser->ended_at->format('F j, Y h:i a') }}</h6>
                         <h6 class="text-center small text-muted">Ends {{ $fundraiser->ended_at->diffForHumans() }}</h6>
                         <div>
-                            <modal-donate title="{{ $fundraiser->name }}" stripe-key="{{ env('STRIPE_PUBLIC_KEY') }}" auth="{{ auth()->check() ? 1 : 0 }}"
+                            <modal-donate title="{{ $fundraiser->fund->name }}" stripe-key="{{ env('STRIPE_PUBLIC_KEY') }}" auth="{{ auth()->check() ? 1 : 0 }}"
                               type="{{ $type or '' }}" type-id="{{ $slug or '' }}" recipient="{{ $fundraiser->sponsor->name }}"></modal-donate>
                         </div>
                     </div>

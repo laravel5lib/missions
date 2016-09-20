f3b114ef605e059bd0c1ae32adbfc38ae348ac3f
Jerez Bain <jerezb31@gmail.com>
2016-09-20 04:15:33 -0400

    $this->fund->balance

--- app/Models/v1/Reservation.php
+++ app/Models/v1/Reservation.php
@@ -267,7 +267,7 @@ class Reservation extends Model
      */
     public function getTotalRaised()
     {
-        return $this->fund->donations()->sum('amount');
+        return $this->fund->balance;
     }
 
     /**

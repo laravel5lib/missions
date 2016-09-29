fb560ea412b60bbc79881b80c75737e3f8b70d6e
Neil Keena <nkeena1@gmail.com>
2016-09-29 13:50:39 -0400

    create and delete trip interests

--- app/Http/Controllers/Api/TripInterestsController.php
+++ app/Http/Controllers/Api/TripInterestsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Requests\v1\TripInterestRequest;
 use App\Http\Transformers\v1\TripInterestTransformer;
 use App\Models\v1\TripInterest;
 use Illuminate\Http\Request;
@@ -26,6 +27,12 @@ class TripInterestsController extends Controller
         $this->interest = $interest;
     }
 
+    /**
+     * Get all trip interests.
+     *
+     * @param Request $request
+     * @return \Dingo\Api\Http\Response
+     */
     public function index(Request $request)
     {
         $interests = $this->interest->filter($request->all())->paginate();
@@ -33,10 +40,44 @@ class TripInterestsController extends Controller
         return $this->response->paginator($interests, new TripInterestTransformer);
     }
 
+    /**
+     * Get trip interest by id.
+     *
+     * @param $id
+     * @return \Dingo\Api\Http\Response
+     */
     public function show($id)
     {
         $interest = $this->interest->findOrFail($id);
 
         return $this->response->item($interest, new TripInterestTransformer);
+    }
+
+    /**
+     * Create a new trip interest.
+     *
+     * @param TripInterestRequest $request
+     * @return \Dingo\Api\Http\Response
+     */
+    public function store(TripInterestRequest $request)
+    {
+        $interest = $this->interest->create($request->all());
+
+        return $this->response->item($interest, new TripInterestTransformer);
+    }
+
+    /**
+     * Delete a trip interest by id.
+     *
+     * @param $id
+     * @return \Dingo\Api\Http\Response
+     */
+    public function destroy($id)
+    {
+        $interest = $this->interest->findOrFail($id);
+
+        $interest->delete();
+
+        return $this->response->noContent();
     }
 }

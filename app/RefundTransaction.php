da31ed8272219b9f7c9e4a27d4c1a85d69657711
Neil Keena <nkeena1@gmail.com>
2016-10-10 09:57:32 -0400

    create a refund transaction

--- app/RefundTransaction.php
+++ app/RefundTransaction.php
@@ -2,12 +2,27 @@
 namespace App;
 
 use App\Http\Requests\v1\Transactions\RefundRequest;
+use Dingo\Api\Contract\Http\Request;
 
-class RefundTransaction
+class RefundTransaction extends TransactionHandler
 {
-    public function create()
+    public function create(Request $request)
     {
         app(RefundRequest::class)->validate();
-        return 'processing refund';
+
+        $refund_id =  $this->merchant->refundCharge(
+            $request->get('charge_id'),
+            $request->get('amount')
+        );
+
+        $transaction = $this->transaction->create([
+            'type' => 'refund',
+            'amount' => $request->get('amount'),
+            'fund_id' => $request->get('fund_id'),
+            'payments' => ['refund_id' => $refund_id],
+            'description' => $request->get('description')
+        ]);
+
+        return $transaction;
     }
 }

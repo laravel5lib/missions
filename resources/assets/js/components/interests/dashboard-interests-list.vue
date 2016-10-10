9402a97c620eebb163bd85c0948b0e757c05c8b8
Jerez Bain <jerezb31@gmail.com>
2016-10-10 15:28:40 -0400

    Interests added as tab with search

--- resources/assets/js/components/interests/dashboard-interests-list.vue
+++ resources/assets/js/components/interests/dashboard-interests-list.vue
@@ -1,12 +1,24 @@
 <template>
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <h5>Trip Interests</h5>
+    <div class="row">
+        <div class="col-sm-12">
+            <form class="form-inline text-right" novalidate>
+                <div class="input-group input-group-sm">
+                    <input type="text" class="form-control" v-model="search" debounce="250" placeholder="Search for anything">
+                    <span class="input-group-addon"><i class="fa fa-search"></i></span>
+                </div>
+                <!--<button class="btn btn-default btn-sm " type="button" @click="showFilters=!showFilters">
+                    Filters
+                    <span class="caret"></span>
+                </button>-->
+                <!--<a class="btn btn-primary btn-sm" href="reservations/create">New <i class="fa fa-plus"></i> </a>-->
+            </form>
         </div>
     </div>
 
-    <div class="">
-        <div class="panel panel-default col-sm-6" v-for="interest in interests" :id="interest.id">
+    <hr class="divider sm">
+
+    <div class="col-sm-6 col-md-4" v-for="interest in interests">
+        <div class="panel panel-default" :id="interest.id">
             <div class="panel-heading"><h6>{{ interest.name }}</h6></div>
             <div class="panel-body">
                 <dl class="dl-hoizontal">
@@ -23,28 +35,24 @@
         </div>
     </div>
 
-        <!--<ul class="media-list col-sm-6">
-            <div class="media" v-for="interest in interests" :id="interest.id">
-                &lt;!&ndash;<div class="media-left">
-                    <a href="#">
-                        <img class="media-object" src="..." alt="...">
+    <div class="col-xs-12 text-center">
+        <nav>
+            <ul class="pagination pagination-md">
+                <li :class="{ 'disabled': pagination.current_page == 1 }">
+                    <a aria-label="Previous" @click="page=pagination.current_page-1">
+                        <span aria-hidden="true">&laquo;</span>
                     </a>
-                </div>&ndash;&gt;
-                <div class="media-body">
-                    <h4 class="media-heading">{{ interest.name }}</h4>
-                    <dl class="dl-horizontal">
-                        <dt v-if="interest.email">Email</dt>
-                        <dd v-if="interest.email"><a :href="'mailto:' + interest.email">{{ interest.email}}</a></dd>
-                        <dt v-if="interest.phone">Phone</dt>
-                        <dd v-if="interest.phone">{{ interest.phone}}</dd>
-                        <dt>Contact Pref.</dt>
-                        <dd>{{ interest.communication_preferences}}</dd>
-                        <dt>Interested since</dt>
-                        <dd>{{ interest.created | moment 'll'}}</dd>
-                    </dl>
-                </div>
-            </div>
-        </ul>-->
+                </li>
+                <li :class="{ 'active': (n+1) == pagination.current_page}" v-for="n in pagination.total_pages"><a @click="page=(n+1)">{{(n+1)}}</a></li>
+                <li :class="{ 'disabled': pagination.current_page == pagination.total_pages }">
+                    <a aria-label="Next" @click="page=pagination.current_page+1">
+                        <span aria-hidden="true">&raquo;</span>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+
 </template>
 <script>
     export default{
@@ -52,17 +60,41 @@
         props: ['groupId', 'tripId'],
         data(){
             return{
-                interests:[]
+                interests:[],
+                // pagination vars
+                search: '',
+                page: 1,
+                per_page: 6,
+                pagination: {},
+
+            }
+        },
+        watch: {
+            'page': function (val, oldVal) {
+                this.pagination.current_page = val;
+                this.searchInterests();
+            },
+            'search': function (val) {
+                this.searchInterests();
+            }
+        },
+        methods: {
+            searchInterests() {
+                this.$http.get('interests', {
+                    group: this.groupId,
+                    trip: this.tripId,
+                    include: '',
+                    search: this.search,
+                    page: this.page,
+                    per_page: this.per_page
+                }).then(function (response) {
+                    this.interests = response.data.data;
+                    this.pagination = response.data.meta.pagination;
+                });
             }
         },
         ready() {
-            this.$http.get('interests', {
-                group: this.groupId,
-                trip: this.tripId,
-                include: ''
-            }).then(function (response) {
-                this.interests = response.data.data;
-            })
+            this.searchInterests();
         }
     }
 </script>

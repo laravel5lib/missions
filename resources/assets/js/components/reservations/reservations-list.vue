41be23e3605df03743514ad725629a23d418137c
Neil Keena <nkeena1@gmail.com>
2016-11-08 20:13:14 -0500

    add dynamic trip type classes to reservation cards

--- resources/assets/js/components/reservations/reservations-list.vue
+++ resources/assets/js/components/reservations/reservations-list.vue
@@ -2,8 +2,8 @@
 <div class="row">
     <div class="col-xs-12 col-sm-6 col-md-4" v-for="reservation in reservations" v-if="reservations.length > 0">
         <div class="panel panel-default">
-            <div class="panel-heading panel-ministry text-center">
-                <h5 class="text-capitalize">{{ reservation.trip.data.type }} Missionary</h5>
+            <div class="panel-heading text-center" :class="'panel-' + reservation.trip.data.type">
+                <h5 class="text-capitalize">{{ reservation.trip.data.type }}</h5>
             </div>
             <div class="panel-body text-center">
                 <img :src="reservation.avatar" class="img-circle img-md">
@@ -13,7 +13,7 @@
                 <label style="margin-bottom:2px;font-size:10px;">Campaign</label>
                 <h6 class="text-capitalize small" style="margin-top:2px;">{{ reservation.trip.data.campaign.data.name }}</h6>
                 <label style="margin-bottom:2px;font-size:10px;">Country</label>
-                <h6 class="text-capitalize small" style="margin-top:2px;">{{ reservation.country }}</h6>
+                <h6 class="text-capitalize small" style="margin-top:2px;">{{ reservation.country_name }}</h6>
                 <hr class="divider inv sm">
                 <a class="btn btn-sm btn-primary" href="/dashboard/reservations/{{ reservation.id }}">View Reservation</a>
                 <hr class="divider inv sm">

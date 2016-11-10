5eb76759a4e32a4e5ce372ea2ea450980c26c1fa
Jerez Bain <jerezb31@gmail.com>
2016-11-10 14:07:36 -0500

    fixed pagination on dashboard reservations

--- resources/assets/js/components/reservations/reservations-list.vue
+++ resources/assets/js/components/reservations/reservations-list.vue
@@ -14,28 +14,34 @@
         </form>
         <hr class="divider sm inv">
     </div>
-    <div class="col-xs-12 col-sm-6 col-md-4" v-for="reservation in reservations" v-if="reservations.length > 0">
-        <div class="panel panel-default">
-            <div class="panel-heading text-center" :class="'panel-' + reservation.trip.data.type">
-                <h5 class="text-capitalize">{{ reservation.trip.data.type }}</h5>
-            </div>
-            <div class="panel-body text-center">
-                <img :src="reservation.avatar" class="img-circle img-md">
-                <hr class="divider inv sm">
-                <h4>{{ reservation.surname }}, {{ reservation.given_names }}</h4>
-                <h6 class="text-capitalize small">{{ reservation.trip.data.group.data.name }}</h6>
-                <label style="margin-bottom:2px;">Campaign</label>
-                <h6 class="text-capitalize small" style="margin-top:2px;">{{ reservation.trip.data.campaign.data.name }}</h6>
-                <label style="margin-bottom:2px;font-size:10px;">Country</label>
-                <h6 class="text-capitalize small" style="margin-top:2px;">{{ reservation.country_name }}</h6>
-                <hr class="divider inv sm">
-                <a class="btn btn-sm btn-primary" href="/dashboard/reservations/{{ reservation.id }}">View Reservation</a>
-                <hr class="divider inv sm">
+    <template v-if="reservations.length > 0">
+        <div class="col-xs-12 col-sm-6 col-md-4" v-for="reservation in reservations">
+            <div class="panel panel-default">
+                <div class="panel-heading text-center" :class="'panel-' + reservation.trip.data.type">
+                    <h5 class="text-capitalize">{{ reservation.trip.data.type }}</h5>
+                </div>
+                <div class="panel-body text-center">
+                    <img :src="reservation.avatar" class="img-circle img-md">
+                    <hr class="divider inv sm">
+                    <h4>{{ reservation.surname }}, {{ reservation.given_names }}</h4>
+                    <h6 class="text-capitalize small">{{ reservation.trip.data.group.data.name }}</h6>
+                    <label style="margin-bottom:2px;">Campaign</label>
+                    <h6 class="text-capitalize small" style="margin-top:2px;">{{ reservation.trip.data.campaign.data.name }}</h6>
+                    <label style="margin-bottom:2px;font-size:10px;">Country</label>
+                    <h6 class="text-capitalize small" style="margin-top:2px;">{{ reservation.country_name }}</h6>
+                    <hr class="divider inv sm">
+                    <a class="btn btn-sm btn-primary" href="/dashboard/reservations/{{ reservation.id }}">View Reservation</a>
+                    <hr class="divider inv sm">
+                </div>
             </div>
         </div>
-    </div>
-    <div class="col-xs-12">
-        <div class="alert alert-info" v-if="reservations.length < 1">No reservations found</div>
+        <div class="col-sm-12 text-center">
+            <pagination :pagination.sync="pagination" :callback="getReservations"></pagination>
+        </div>
+    </template>
+
+    <div class="col-xs-12" v-if="reservations.length < 1">
+        <div class="alert alert-info">No reservations found</div>
     </div>
 </div>
 </template>
@@ -46,6 +52,7 @@
         data(){
             return {
                 reservations: [],
+                pagination: { current_page: 1},
                 trips: [],
                 includeManaging: false,
                 search: '',
@@ -67,28 +74,28 @@
             getReservations(){
                 var params = {
                     include: 'trip.campaign,trip.group',
-                    search: this.search
+                    search: this.search,
+                    page: this.pagination.current_page
                 }
+
                 if (this.includeManaging) {
                     params.trip = this.trips;
                 } else {
                     params.user = new Array(this.userId);
                 }
 
-                this.$http.get('reservations', params).then(function (response) {
+                switch (this.type) {
+                    case 'active':
+                        params.current = true;
+                        break;
+                    case 'archive':
+                        params.archived = true;
+                        break;
+                }
 
-                    switch (this.type) {
-                        case 'active':
-                            this.reservations = _.filter(response.data.data, function (reservation) {
-                                return moment().isBefore(reservation.trip.data.ended_at);
-                            });
-                            break;
-                        case 'archive':
-                            this.reservations = _.filter(response.data.data, function (reservation) {
-                                return moment().isAfter(reservation.trip.data.ended_at);
-                            });
-                            break;
-                    }
+                this.$http.get('reservations', params).then(function (response) {
+                    this.reservations = response.data.data
+                    this.pagination = response.data.meta.pagination;
                 });
             },
         },

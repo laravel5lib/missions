4696c5a6946081c57c201574971c93768aa48e33
Neil Keena <nkeena1@gmail.com>
2016-11-12 11:10:27 -0500

    clean up seeder to fix duplicates bug

--- database/seeds/ReservationTableSeeder.php
+++ database/seeds/ReservationTableSeeder.php
@@ -1,8 +1,6 @@
 <?php
 
-use App\Events\ReservationWasCreated;
 use Illuminate\Database\Seeder;
-use Illuminate\Http\Request;
 
 class ReservationTableSeeder extends Seeder
 {
@@ -13,7 +11,8 @@ class ReservationTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Models\v1\Reservation::class, config('seeders.reservations'))->create()->each(function($r) {
+        factory(App\Models\v1\Reservation::class, config('seeders.reservations'))->create()->each(function($r)
+        {
 
             $r->companions()->save(factory(App\Models\v1\Companion::class)->make());
 
@@ -26,22 +25,9 @@ class ReservationTableSeeder extends Seeder
 
             $this->addDependencies($r);
         });
-
-        // Give the admin user at least one reservation.
-        $user = App\Models\v1\User::where('name', 'Admin')->firstOrFail();
-
-        factory(App\Models\v1\Reservation::class)->create(['user_id' => $user->id])->each(function($ar)
-        {
-
-            $ar->companions()->save(factory(App\Models\v1\Companion::class)->make());
-
-            $ar->notes()->save(factory(App\Models\v1\Note::class)->make());
-
-            $this->addDependencies($ar);
-        });
     }
 
-    protected function addDependencies($res)
+    private function addDependencies($res)
     {
         $active = $res->trip->activeCosts()->get();
 
@@ -102,4 +88,5 @@ class ReservationTableSeeder extends Seeder
             'ended_at' => $res->trip->started_at
         ]);
     }
+
 }

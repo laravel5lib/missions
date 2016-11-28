8e0097103afb9052fedde4d9b0f7e1f340f93d48
Neil Keena <nkeena1@gmail.com>
2016-11-28 15:47:43 -0500

    Add a button to see public trip page

--- resources/views/admin/trips/tabs/description.blade.php
+++ resources/views/admin/trips/tabs/description.blade.php
@@ -1,11 +1,12 @@
 <div class="panel panel-default">
     <div class="panel-heading">
         <div class="row">
-            <div class="col-xs-8">
+            <div class="col-xs-6">
                 <h5>Description</h5>
             </div>
-            <div class="col-xs-4 text-right">
-                <action-trigger text="Toggle Edit/Preview" icon="fa fa-edit" event="toggleMode" size="btn-sm"></action-trigger>
+            <div class="col-xs-6 text-right">
+                <a class="btn btn-sm btn-default" href="{{ url('trips/' . $trip->id) }}" target="_blank">See Public Page</a>
+                <action-trigger text="Edit / Preview" event="toggleMode" size="btn-sm"></action-trigger>
             </div>
         </div>
     </div>

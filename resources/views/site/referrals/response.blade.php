58e5ac765b3acae7fae71ee6f08221d8f80a3996
Neil Keena <nkeena1@gmail.com>
2016-12-20 17:08:59 -0500

    fix name on referral response and add disclaimer

--- resources/views/site/referrals/response.blade.php
+++ resources/views/site/referrals/response.blade.php
@@ -7,10 +7,13 @@
             <div class="col-md-offset-2 col-md-8">
                 <div class="panel panel-default">
                     <div class="panel-heading">
-                        <h5>Referral Request from {{ $referral->user->name }}</h5>
+                        <h5>Referral Request from {{ $referral->name }}</h5>
                     </div>
                     <div class="panel-body">
                         <referral-response id="{{ $referral->id }}"></referral-response>
+                    </div>
+                    <div class="panel-footer">
+                        <p class="small text-muted text-center">*This information is kept strictly confidential and will not be shared with the applicant. Your reference is reviewed by Missions.Me staff only.</p>
                     </div>
                 </div>
             </div>

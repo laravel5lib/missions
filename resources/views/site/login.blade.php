7e8678f6fe982242022473904944644daa19483c
zechnelson <hello@zechnelson.com>
2017-01-04 15:15:45 -0500

    Login screen background

--- resources/views/site/login.blade.php
+++ resources/views/site/login.blade.php
@@ -1,18 +1,28 @@
 @extends('site.layouts.default')
 
 @section('content')
-<div class="gray-lighter-bg">
+<div class="login-bg">
   <div class="container">
     <div class="row">
       <hr class="divider inv xlg">
       <hr class="divider inv xlg">
-      <div class="col-md-6 col-md-offset-3">
+      <hr class="hidden-xs divider inv xlg">
+      <div class="col-xs-12 col-xs-offset-0 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 col-lg-4 col-lg-offset-4">
         <h5 class="text-uppercase text-center">Welcome To Missions.Me</h5>
         <hr class="divider inv">
           <login></login>
         <hr class="divider inv xlg">
         <hr class="divider inv xlg">
-        </div>
+        <hr class="divider inv xlg">
+        <hr class="visible-xs divider inv xlg">
+        <hr class="visible-xs divider inv xlg">
+        <hr class="visible-xs divider inv xlg">
+        <hr class="visible-xs divider inv xlg">
+        <hr class="visible-xs divider inv xlg">
+        <hr class="visible-xs divider inv xlg">
+        <hr class="visible-xs divider inv xlg">
+        <hr class="visible-xs divider inv xlg">
+        <hr class="visible-xs divider inv xlg">
       </div><!-- end col -->
     </div><!-- end row -->
   </div><!-- end container -->

444d340776735fe4dba60001209664fa55816942
Neil Keena <nkeena1@gmail.com>
2017-01-05 10:37:56 -0500

    add new permissions, remove unused tabs from trip details, add requirement edit modal
    database re-seed for permissions

--- database/seeds/BouncerSeeder.php
+++ database/seeds/BouncerSeeder.php
@@ -113,5 +113,10 @@ class BouncerSeeder extends Seeder
         Bouncer::allow('admin')->to('modify', \App\Models\v1\Todo::class);
         // notes
         Bouncer::allow('admin')->to('modify', \App\Models\v1\Note::class);
+        // uploads
+        Bouncer::allow('admin')->to('view', \App\Models\v1\Upload::class);
+        Bouncer::allow('admin')->to('create', \App\Models\v1\Upload::class);
+        Bouncer::allow('admin')->to('edit', \App\Models\v1\Upload::class);
+        Bouncer::allow('admin')->to('delete', \App\Models\v1\Upload::class);
     }
 }

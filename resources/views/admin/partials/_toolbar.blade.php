f3d2b2f09fa0f47ed2ab0e1e36eb660f0facfdad
zechnelson <hello@zechnelson.com>
2017-01-06 13:54:14 -0500

    nav active fix

--- resources/views/admin/partials/_toolbar.blade.php
+++ resources/views/admin/partials/_toolbar.blade.php
@@ -1,31 +1,31 @@
 <div id="myNavmenu" class="navmenu navmenu-inverse navmenu-fixed-left offcanvas-sm">
   <ul class="nav navmenu-nav">
-    <li class="offcanvas-tooltip" @if(request()->segment(2) == '')class="active"@endif> 
-        <a href="/admin"><i class="fa fa-tachometer"></i><span>Dashboard</span></a>
+    <li @if(request()->segment(2) == '')class="active"@endif> 
+        <a href="/admin"><i class="fa fa-tachometer"></i></a>
     </li>
-    <li class="offcanvas-tooltip" @if(in_array(request()->segment(2), ['campaigns', 'trips']))class="active"@endif>
-      <a href="/admin/campaigns"><i class="fa fa-globe"></i><span>Campaigns</span></a>
+    <li @if(in_array(request()->segment(2), ['campaigns', 'trips']))class="active"@endif>
+      <a href="/admin/campaigns"><i class="fa fa-globe"></i></a>
     </li>
-    <li class="offcanvas-tooltip" @if(in_array(request()->segment(2), ['reservations', 'interests']))class="active"@endif>
-      <a href="/admin/reservations/current"><i class="fa fa-ticket"></i><span>Reservations</span></a>
+    <li @if(in_array(request()->segment(2), ['reservations', 'interests']))class="active"@endif>
+      <a href="/admin/reservations/current"><i class="fa fa-ticket"></i></a>
     </li>
-    <li class="offcanvas-tooltip" @if(request()->segment(2) == 'groups')class="active"@endif>
-      <a href="/admin/groups"><i class="fa fa-users"></i><span>Groups</span></a>
+    <li @if(request()->segment(2) == 'groups')class="active"@endif>
+      <a href="/admin/groups"><i class="fa fa-users"></i></a>
     </li>
-    <li class="offcanvas-tooltip" @if(request()->segment(2) == 'users')class="active"@endif>
-      <a href="/admin/users"><i class="fa fa-user"></i><span>Users</span></a>
+    <li @if(request()->segment(2) == 'users')class="active"@endif>
+      <a href="/admin/users"><i class="fa fa-user"></i></a>
     </li>
-    <li class="offcanvas-tooltip" @if(request()->segment(2) == 'uploads')class="active"@endif>
-      <a href="/admin/uploads"><i class="fa fa-picture-o"></i><span>Uploads</span></a>
+    <li @if(request()->segment(2) == 'uploads')class="active"@endif>
+      <a href="/admin/uploads"><i class="fa fa-picture-o"></i></a>
     </li>
-    <li class="offcanvas-tooltip" @if(in_array(request()->segment(2), ['funds', 'transactions', 'donors']))class="active"@endif>
-      <a href="/admin/funds"><i class="fa fa-usd" style="margin-left:3px;"></i><span>Funds</span></a>
+    <li @if(in_array(request()->segment(2), ['funds', 'transactions', 'donors']))class="active"@endif>
+      <a href="/admin/funds"><i class="fa fa-usd" style="margin-left:3px;"></i></a>
     </li>
-    <li class="offcanvas-tooltip" @if(in_array(request()->segment(2), ['causes', 'projects', 'initiatives']))class="active"@endif>
-        <a href="/admin/causes"><i class="fa fa-tint" style="margin-left:3px;"></i><span>Projects</span></a>
+    <li @if(in_array(request()->segment(2), ['causes', 'projects', 'initiatives']))class="active"@endif>
+        <a href="/admin/causes"><i class="fa fa-tint" style="margin-left:3px;"></i></a>
     </li>
-    <li class="offcanvas-tooltip" @if(request()->segment(2) == 'records')class="active"@endif>
-      <a href="/admin/records"><i class="fa fa-archive"></i><span>Records</span></a>
+    <li @if(request()->segment(2) == 'records')class="active"@endif>
+      <a href="/admin/records"><i class="fa fa-archive"></i></a>
     </li>
   </ul>
 </div>
--- resources/views/dashboard/partials/_toolbar.blade.php
+++ resources/views/dashboard/partials/_toolbar.blade.php
@@ -1,24 +1,24 @@
 <div id="myNavmenu" class="navmenu navmenu-inverse navmenu-fixed-left offcanvas-sm">
   <ul class="nav navmenu-nav">
-    <li class="offcanvas-tooltip" @if(request()->segment(2) == '')class="active"@endif><a href="/dashboard">
-        <i class="fa fa-tachometer"></i><span>Dashboard</span></a>
+    <li @if(request()->segment(2) == '')class="active"@endif><a href="/dashboard">
+        <i class="fa fa-tachometer"></i></a>
     </li>
-    <li class="offcanvas-tooltip" @if(request()->segment(2) == 'settings')class="active"@endif>
-      <a href="/dashboard/settings"><i class="fa fa-cog"></i><span>Settings</span></a>
+    <li @if(request()->segment(2) == 'settings')class="active"@endif>
+      <a href="/dashboard/settings"><i class="fa fa-cog"></i></a>
     </li>
-    <li class="offcanvas-tooltip" @if(request()->segment(2) == 'reservations')class="active"@endif>
-      <a href="/dashboard/reservations"><i class="fa fa-ticket"></i><span>Reservations</span></a>
+    <li @if(request()->segment(2) == 'reservations')class="active"@endif>
+      <a href="/dashboard/reservations"><i class="fa fa-ticket"></i></a>
     </li>
-    <li class="offcanvas-tooltip" @if(request()->segment(2) == 'records')class="active"@endif>
-      <a href="/dashboard/records"><i class="fa fa-archive"></i><span>Records</span></a>
+    <li @if(request()->segment(2) == 'records')class="active"@endif>
+      <a href="/dashboard/records"><i class="fa fa-archive"></i></a>
     </li>
     @if(auth()->user()->managing()->count())
-    <li class="offcanvas-tooltip" @if(request()->segment(2) == 'groups')class="active"@endif>
-      <a href="/dashboard/groups"><i class="fa fa-users"></i><span>Groups</span></a>
+    <li @if(request()->segment(2) == 'groups')class="active"@endif>
+      <a href="/dashboard/groups"><i class="fa fa-users"></i></a>
     </li>
     @endif
-    <li class="offcanvas-tooltip" @if(request()->segment(2) == 'projects')class="active"@endif>
-      <a href="/dashboard/projects"><i class="fa fa-tint" style="margin-left:3px;"></i><span>Projects</span></a>
+    <li @if(request()->segment(2) == 'projects')class="active"@endif>
+      <a href="/dashboard/projects"><i class="fa fa-tint" style="margin-left:3px;"></i></a>
     </li>
   </ul>
 </div>

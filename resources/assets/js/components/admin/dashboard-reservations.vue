2a8b3de0956099bbf33337bba42de2c574021a62
Jerez Bain <jerezb31@gmail.com>
2017-01-10 17:20:38 -0500

    dashboard reservations component (if we ever need one...)

---
+++ resources/assets/js/components/admin/dashboard-reservations.vue
@@ -0,0 +1,53 @@
+<template>
+	<div class="panel panel-default">
+		<spinner v-ref:spinner size="sm" text="Loading"></spinner>
+		<div class="panel-heading">
+			<h5>Newest Reservations</h5>
+		</div>
+		<table class="table table-hover">
+			<tbody>
+			@foreach($reservations->current()->latest()->take(5)->get() as $reservation)
+			<tr onclick="window.location.href = 'admin/reservations/{{ $reservation->id }}'" style="cursor: pointer;">
+				<td style="padding:5px 15px;vertical-align:middle;font-size:12px;">{{ $reservation->given_names }}</td>
+				<td style="padding:5px 15px;vertical-align:middle;font-size:12px;">
+					{{ $reservation->trip->campaign->name }}
+					<br /><small class="text-muted">{{ $reservation->trip->group->name }}</small>
+				</td>
+				<td style="padding:5px 15px;vertical-align:middle;line-height:12px;">
+					<small class="text-muted" style="font-size:10px;">{{ $reservation->created_at->diffForHumans() }}</small>
+				</td>
+			</tr>
+			@endforeach
+			</tbody>
+		</table>
+		<div class="panel-footer text-center" style="padding:10px;">
+			<a class="small" style="color:#bcbcbc;" href="{{ url('/admin/reservations/current') }}">View All Reservations</a>
+		</div>
+	</div>
+</template>
+<script type="text/javascript">
+    export default{
+    	name: 'dashboard-reservations',
+        data(){
+            return{
+                reservations: []
+            }
+        },
+		methods: {
+            getReservations() {
+				this.$http.get('reservations', {
+					current: true,
+					per_page: 5,
+					sort: 'created_at+DESC',
+					page: 1
+				}).then(function (response) {
+					this.reservations = response.data.data;
+				})
+			}
+		},
+        ready(){
+            this.getReservations();
+        }
+    }
+
+</script>

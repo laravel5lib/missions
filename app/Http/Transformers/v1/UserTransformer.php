9c709ceff2cd986c67dab55c45ca668ef0fbaf49
Jerez Bain <jerezb31@gmail.com>
2017-01-18 13:15:30 -0500

    add desired_role to user transformer and make editable in reservations

--- app/Http/Transformers/v1/UserTransformer.php
+++ app/Http/Transformers/v1/UserTransformer.php
@@ -45,6 +45,7 @@ class UserTransformer extends TransformerAbstract
             'zip'          => $user->zip,
             'country_code' => $user->country_code,
             'country_name' => country($user->country_code),
+            'desired_role' => $user->desired_role,
             'timezone'     => $user->timezone,
             'bio'          => $user->bio,
             'url'          => $user->slug ? $user->slug->url : null,

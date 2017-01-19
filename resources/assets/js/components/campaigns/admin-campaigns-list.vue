513b40c70a44d3236d8bab2df9dcd3e92ac484b9
Jerez Bain <jerezb31@gmail.com>
2017-01-19 13:19:39 -0500

    admin campaign component created

---
+++ resources/assets/js/components/campaigns/admin-campaigns-list.vue
@@ -0,0 +1,31 @@
+<template>
+    <div> 
+        <div>this is template body</div>
+    </div>
+</template>
+<script type="text/javascript">
+    import vSelect from "vue-select";
+    export default{
+    	name: 'admin-campaigns-list',
+        components: {vSelect},
+        data(){
+            return{
+                campaigns: [],
+                orderByField: 'surname',
+                direction: 1,
+                page: 1,
+                per_page: 10,
+                perPageOptions: [5, 10, 25, 50, 100],
+                pagination: {current_page: 1},
+                search: '',
+
+            }
+        },
+        methods: {
+
+        },
+        ready(){
+            
+        }
+    }
+</script>

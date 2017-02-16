000ad6b389d115bfeeb537f9dc7614f0b0341231
zechnelson <hello@zechnelson.com>
2017-02-16 17:44:45 -0500

    india campaign

--- resources/views/site/campaigns/partials/_india.blade.php
+++ resources/views/site/campaigns/partials/_india.blade.php
@@ -1,5 +1,5 @@
 <div class="content-page-header">
-    <img class="img-responsive" src="../images/india/christmas-india.jpg" alt="Christmas In India">
+    <img class="img-responsive" src="../images/india/orphans-to-angels.jpg" alt="Orphans To Angels">
     <div class="c-page-header-text">
     </div><!-- end c-page-header-content -->
 </div><!-- end c-page-header -->
@@ -8,9 +8,14 @@
 	<div class="content-section">
 		<div class="row">
 			<div class="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2 text-center">
+				<div class="video-outer">
+					<div class="video-inner">
+						<iframe src="https://player.vimeo.com/video/187513568?title=0&byline=0&portrait=0" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+					</div>
+				</div>
 				<hr data-aos="fade-up" class="divider red-small">
 				<hr class="divider inv">
-				<p data-aos="fade-up">Welcome to the other side of the earth. It's difficult to find a place further away from home before you start coming back. Submerge yourself in an incomparable culture, from the sites, sounds, smells, food, people and clothing - India is an experience for all the senses. Venture deep into southern India as together we Rescue EVERY Child in several villages in the state of Andhra Pradesh. Watch as they enjoy their first Christmas and shower them with more Christmas gifts than their little arms can hold.</p>
+				<p data-aos="fade-up">Welcome to the other side of the earth. It's difficult to find a place further away from home before you start coming back. Submerge yourself in an incomparable culture, from the sites, sounds, smells, food, people and clothing - India is an amazing experience. Venture deep into southern India as together we rescue children in several villages in the state of Andhra Pradesh.</p>
 			</div><!-- end col -->
 		</div><!-- end row -->
 	</div>
@@ -25,7 +30,8 @@
 				<h1 class="text-uppercase" data-aos="fade-up">What You'll Be Doing</h1>
 				<hr class="divider red-small">
 				<hr class="divider inv">
-				<p data-aos="fade-up">Participate in the emotional dedications of several orphanages, each permanently houses 12-50 orphans. Spend the day at each Angel House loving on each kid who may have never been hugged or loved on before.</p>
+				<p data-aos="fade-up">Participate in the emotional dedications of several orphanages, each permanently houses 12-50 orphans.</p>
+				<p>Spend the day at each "Angel House" loving on each kid who may have never been hugged or loved on in their entire lives.</p>
 			</div><!-- end col -->
 		</div><!-- end row -->
 	</div>

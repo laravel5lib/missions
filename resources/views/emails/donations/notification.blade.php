be9fcd0552f7e96f4f9aa92495cec3c0bbb8ed14
Neil Keena <nkeena1@gmail.com>
2017-02-24 16:26:55 -0500

    fix dollar formatting in emails

--- resources/views/emails/donations/notification.blade.php
+++ resources/views/emails/donations/notification.blade.php
@@ -6,6 +6,6 @@
 
     <p style="color: #242424;font-family:Helvetica, Arial, sans-serif;"><span style="color:#3e3e3e;font-weight:bold;">{{ $donation->donor->name }}</span> donated <span style="color:#05ce7b;font-weight:bold;">${{ $donation->amount }}</span> to <span style="color:#3e3e3e;font-weight:bold;">{{ $donation->fund->name }}</span></p>
 
-    <p style="color: #242424;font-family:Helvetica, Arial, sans-serif;">You have successfully raised a total of <span style="color:#05ce7b;font-weight:bold;">${{ $donation->fund->balance }}</span></p>
+    <p style="color: #242424;font-family:Helvetica, Arial, sans-serif;">You have successfully raised a total of <span style="color:#05ce7b;font-weight:bold;">${{ $donation->fund->balanceInDollars() }}</span></p>
 
 @stop

7ac89527b229945a3e748c58af1e6c5293a4ab13
Neil Keena <nkeena1@gmail.com>
2017-02-25 21:25:32 -0500

    make fundraiser private on creation by default; improve slug generator

--- app/Jobs/Reservations/SetupFunding.php
+++ app/Jobs/Reservations/SetupFunding.php
@@ -43,17 +43,18 @@ class SetupFunding extends Job
             'goal_amount' => $this->reservation->getTotalCost(),
             'started_at' => $this->reservation->created_at,
             'ended_at' => $this->reservation->trip->started_at,
-            'banner_upload_id' => $this->reservation->trip->campaign->banner->id
+            'banner_upload_id' => $this->reservation->trip->campaign->banner->id,
+            'public' => false // private by default
         ]);
     }
 
     protected function makeSlug()
     {
-        $slug = str_slug(country($this->reservation->trip->country_code)).
-                '-'.
-                $this->reservation->trip->started_at->format('Y').
-                '-'.
-                str_random(4);
+        $slug = generate_fundraiser_slug(
+                    country($this->reservation->trip->country_code)).
+                    '-'.
+                    $this->reservation->trip->started_at->format('Y')
+                )
 
         return $slug;
     }

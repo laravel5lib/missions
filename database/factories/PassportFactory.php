84579b1886232f003b745d63072f843a5f270b51
Neil Keena <nkeena1@gmail.com>
2017-03-06 16:22:59 -0500

    passports endpoint acceptance test

--- database/factories/PassportFactory.php
+++ database/factories/PassportFactory.php
@@ -8,16 +8,12 @@ $factory->define(App\Models\v1\Passport::class, function (Faker\Generator $faker
     return [
         'given_names'   => $faker->firstName . ' ' . $faker->firstName,
         'surname'       => $faker->lastName,
-        'number'        => $faker->randomNumber(9),
+        'number'        => (string) $faker->randomNumber(9),
         'expires_at'    => \Carbon\Carbon::now()->addYears(10),
         'birth_country' => strtolower($faker->countryCode),
         'citizenship'   => strtolower($faker->countryCode),
-        'user_id'       => function () {
-            return factory(App\Models\v1\User::class)->create()->id;
-        },
-        'upload_id'     => function() {
-            return factory(App\Models\v1\Upload::class, 'passport')->create()->id;
-        }
+        'user_id'       => $faker->uuid,
+        'upload_id'     => null
     ];
 });
 

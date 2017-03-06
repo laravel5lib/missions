e19af3094174cf2807c14d961176b0ad44b4ef3b
Jerez Bain <jerezb31@gmail.com>
2017-03-06 16:23:31 -0500

    date-picker date only for visa and passport

--- resources/assets/js/components/records/passports/passport-create-update.vue
+++ resources/assets/js/components/records/passports/passport-create-update.vue
@@ -36,7 +36,7 @@
                         <div class="col-lg-6">
                             <div class="input-group input-group-sms"
                                  :class="{ 'has-error': checkForError('expires') }">
-                                <date-picker class="form-control input-sms" :time.sync="expires_at|moment 'YYYY-MM-DD HH:mm:ss'"></date-picker>
+                                <date-picker class="form-control input-sms" :time.sync="expires_at" :option="{ type: 'day', format: 'YYYY-MM-DD' }"></date-picker>
                                 <input type="datetime" class="form-control hidden" v-model="expires_at" id="expires_at" :min="tomorrow"
                                        v-validate:expires="{ required: true }" required>
                             </div>

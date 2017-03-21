afa59dfe4985c6188de67483cc4bae26a64cae77
Jerez Bain <jerezb31@gmail.com>
2017-03-21 10:40:14 -0400

    hot-fix for birthday date issue #424

--- resources/assets/js/components/trips/trip-registration-wizard.vue
+++ resources/assets/js/components/trips/trip-registration-wizard.vue
@@ -221,7 +221,7 @@
 					gender: this.userInfo.gender,
 					status: this.userInfo.relationshipStatus,
 					shirt_size: this.userInfo.size,
-					birthday: moment().set({year: this.userInfo.dobYear, month: this.userInfo.dobMonth, day: this.userInfo.dobDay}).format('YYYY-MM-DD'),
+					birthday: moment(this.userInfo.dobMonth + '-' + this.userInfo.dobDay + '-' + this.userInfo.dobYear, 'MM-DD-YYYY').format('YYYY-MM-DD'),
 					address: this.userInfo.address,
 					city: this.userInfo.city,
 					state: this.userInfo.state,

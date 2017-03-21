f694f231a208b29519ad4e2fd1166eeafbf8a5dd
Neil Keena <nkeena1@gmail.com>
2017-03-21 11:27:53 -0400

    fix typo

--- resources/views/admin/trips/tabs/details.blade.php
+++ resources/views/admin/trips/tabs/details.blade.php
@@ -119,7 +119,7 @@
             </div>
             <div class="col-xs-6 col-sm-6 col-md-4 text-center">
                 <label>Registration Closes</label>
-                <h4>{{ $trip->closes_at->format('M j, Y h:i a') }}</h4>
+                <h4>{{ $trip->closed_at->format('M j, Y h:i a') }}</h4>
             </div>
             <div class="col-xs-6 col-sm-6 col-md-4 text-center">
                 <label>Starting Cost</label>
--- resources/views/dashboard/groups/trips/tabs/registration.blade.php
+++ resources/views/dashboard/groups/trips/tabs/registration.blade.php
@@ -10,8 +10,8 @@
             </div>
             <div class="col-xs-6 col-sm-6 col-md-4 text-center">
                 <label>Registration Closes</label>
-                <h4>{{ $trip->closes_at->diffForHumans() }}</h4>
-                <small>{{ $trip->closes_at->format('M j, Y h:i a') }}</small>
+                <h4>{{ $trip->closed_at->diffForHumans() }}</h4>
+                <small>{{ $trip->closed_at->format('M j, Y h:i a') }}</small>
             </div>
             <div class="col-xs-6 col-sm-6 col-md-4 text-center">
                 <label>Starting Cost</label>

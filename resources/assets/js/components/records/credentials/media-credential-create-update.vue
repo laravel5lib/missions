273ba850f140788511bd2611285d63b1eec11277
Jerez Bain <jerezb31@gmail.com>
2017-04-08 07:28:07 -0400

    add dynamicValidity checking to equipment checkboxes

--- resources/assets/js/components/records/credentials/media-credential-create-update.vue
+++ resources/assets/js/components/records/credentials/media-credential-create-update.vue
@@ -137,9 +137,9 @@
 												<label>
 													<input type="checkbox" :value="choice.value" v-model="choice.value"> {{ choice.name }}
                                                 </label>
-												<div class="well well-sm" :class="{'hidden': !choice.value}">
+												<div v-show="choice.value" :class="{'has-error': !choice.brand}">
 													<label>BRAND/MODEL</label>
-													<input class="form-control" type="text" v-model="choice.brand">
+													<input class="form-control input-sm " type="text" v-model="choice.brand">
 												</div>
 											</div>
 										</template>
@@ -375,6 +375,7 @@
             validateDynamically(){
                 let self = this;
                 let pass = true;
+
                 // check proficiencies
                 let roles = _.findWhere(this.content, { id: 'role'});
                 _.each(roles.options, function (role) {
@@ -383,6 +384,17 @@
                         pass = false;
                     }
                 });
+
+                // check brands/models
+                let equipment = _.findWhere(this.content, { id: 'equipment'});
+                _.each(equipment.options, function (item) {
+                    if (item.brand === null || item.brand === '') {
+                        self.$root.$emit('showError', 'Please type a brand/model name for your ' + item.name);
+                        pass = false;
+                    }
+                });
+
+
                 return pass;
             }
         },

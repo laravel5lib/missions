d0ddd2f40c356bdca69ce32f0ad297e0500b768f
Jerez Bain <jerezb31@gmail.com>
2017-05-02 16:57:30 -0400

    minor functionality

--- resources/assets/js/components/rooms/rooming-manager.vue
+++ resources/assets/js/components/rooms/rooming-manager.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="row" style="position:relative;">
 		<spinner v-ref:spinner size="sm" text="Loading"></spinner>
-		<div class="col-sm-3">
+		<div class="col-sm-4">
 			<div class="col-xs-12 text-right">
 				<button class="btn btn-primary btn-xs" @click="openNewPlanModel">Create A Plan</button>
 				<hr class="divider lg">
@@ -75,7 +75,7 @@
 			</div>
 
 		</div>
-		<div class="col-sm-6">
+		<div class="col-sm-4">
 			<div class="panel panel-default">
 				  <div class="panel-heading">
 						<h3 class="panel-title">Occupants</h3>
@@ -85,10 +85,79 @@
 				  </div>
 			</div>
 		</div>
-		<div class="col-sm-3">
-			<select class="form-control" v-model="currentTeam">
-				<option :value="team" v-for="team in teams">{{team.name | capitalize}}</option>
+		<div class="col-sm-4">
+			<select class="form-control input-sm" v-model="currentTeam">
+				<option :value="" v-if="!currentTeam">Select Team</option>
+				<option :value="team" v-for="team in teams">{{team.callsign | capitalize}}</option>
 			</select>
+
+			<div class="panel panel-default" v-for="member in currentTeamMembers">
+				<div class="panel-heading" role="tab" id="headingOne">
+					<h4 class="panel-title">
+						<div class="row">
+							<div class="col-xs-9">
+								<a role="button" data-toggle="collapse" :data-parent="'#membersAccordion' + tgIndex" :href="'#memberItem' + tgIndex + $index" aria-expanded="true" aria-controls="collapseOne">
+									<img :src="member.avatar" class="img-circle img-xs pull-left" style="margin-right: 10px">
+									{{ member.surname | capitalize }}, {{ member.given_names | capitalize }} <span class="label label-info" v-if="member.leader">Group Leader</span><br>
+									<label>{{ member.desired_role.name }}</label>
+								</a>
+							</div>
+							<div class="col-xs-3 text-right action-buttons">
+								<dropdown type="default">
+									<button slot="button" type="button" class="btn btn-xs btn-primary-hollow dropdown-toggle">
+										<span class="fa fa-ellipsis-h"></span>
+									</button>
+									<ul slot="dropdown-menu" class="dropdown-menu dropdown-menu-right">
+										<template v-for="subSquad in currentSquads">
+											<template v-if="subSquad.callsign === 'Team Leaders'">
+												<li :class="{'disabled': isLocked}" v-if="canAssignToSquadLeader(member)"><a @click="moveToSquad(member, squad, subSquad, true)" v-text="'Move to ' + subSquad.callsign + ''"></a></li>
+												<li :class="{'disabled': isLocked}" v-if="canAssignToSquad(member)"><a @click="moveToSquad(member, squad, false)" v-text="'Move to ' + subSquad.callsign"></a></li>
+											</template>
+											<template v-else>
+												<template v-if="subSquad.id !== squad.id">
+													<li :class="{'disabled': isLocked}" v-if="canAssignToTeamLeaders(member)"><a @click="moveToSquad(member, squad, subSquad, false)">Move to Team Leaders</a></li>
+													<li :class="{'disabled': isLocked}" v-if="canAssignToSquadLeader(member)"><a @click="moveToSquad(member, squad, subSquad, true)" v-text="'Move to ' + subSquad.callsign + ' as leader'"></a></li>
+													<li :class="{'disabled': isLocked}" v-if="canAssignToSquad(member)"><a @click="moveToSquad(member, squad, subSquad, false)" v-text="'Move to ' + subSquad.callsign"></a></li>
+												</template>
+											</template>
+										</template>
+										<li :class="{'disabled': isLocked}" role="separator" class="divider"></li>
+										<li :class="{'disabled': isLocked}" v-if="member.leader"><a @click="demoteToMember(member, squad)">Demote to Group Member</a></li>
+										<li :class="{'disabled': isLocked}" v-if="!member.leader && !squadHasLeader(squad)"><a @click="promoteToLeader(member, squad)">Promote to Group Leader</a></li>
+										<li :class="{'disabled': isLocked}"><a @click="removeFromSquad(member, squad)">Remove</a></li>
+									</ul>
+								</dropdown>
+								<a class="btn btn-xs btn-default-hollow" role="button" data-toggle="collapse" data-parent="#membersAccordion" :href="'#memberItem' + tgIndex + $index" aria-expanded="true" aria-controls="collapseOne">
+									<i class="fa fa-angle-down"></i>
+								</a>
+							</div>
+						</div>
+					</h4>
+				</div>
+				<div :id="'memberItem' + tgIndex + $index" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
+					<div class="panel-body">
+						<div class="row">
+							<div class="col-sm-6">
+								<label>Gender</label>
+								<p class="small">{{member.gender | capitalize}}</p>
+								<label>Marital Status</label>
+								<p class="small">{{member.status | capitalize}}</p>
+							</div><!-- end col -->
+							<div class="col-sm-6">
+								<label>Age</label>
+								<p class="small">{{member.age}}</p>
+								<label>Travel Group</label>
+								<p class="small">{{member.trip.data.group.data.name}}</p>
+							</div><!-- end col -->
+						</div><!-- end row -->
+					</div><!-- end panel-body -->
+				</div>
+				<div class="panel-footer" style="background-color: #ffe000;" v-if="member.companions.data.length && companionsPresentSquad(member, squad)">
+					<i class=" fa fa-info-circle"></i> I have {{member.present_companions}} companions not in this group. And {{companionsPresentTeam(member)}} not on this team.
+					<button type="button" class="btn btn-xs btn-default-hollow" @click="addCompanionsToSquad(member, squad)">Add Companions</button>
+				</div>
+			</div>
+
 		</div>
 	</div>
 </template>
@@ -111,6 +180,8 @@
         },
         data(){
             return {
+                startUp: true,
+
                 plans: [],
                 plansPagination: { current_page: 1 },
                 rooms: [],
@@ -125,12 +196,24 @@
                 currentTeam: null,
                 currentPlan: null,
 				activeRooms: [],
+                roomTypes: [],
 
                 // Filters vars
                 showReservationsFilters: false,
             }
         },
-	    computed: {},
+	    computed: {
+            currentTeamMembers() {
+                let members = [];
+                if (_.isObject(this.currentTeam))
+	                _.each(this.currentTeam.squads.data, function (squad) {
+						_.each(squad.members.data, function (member) {
+							members.push(member);
+	                    });
+	                });
+                return members;
+            }
+	    },
         methods: {
             searchReservations(){
                 let params = {
@@ -166,9 +249,32 @@
                     //TODO add error alert
                 });
             },
+            getRoomTypes(){
+                return this.$http.get('rooms/types').then(function (response) {
+                        return this.roomTypes = response.body.data;
+                    },
+                    function (response) {
+                        console.log(response);
+                        return response.body.data;
+                    });
+            },
+            getPlans(){
+                let params = {
+                    // include: '',
+                    page: this.plansPagination.current_page,
+                };
+                return this.$http.get('rooming-plans', { params: params }).then(function (response) {
+                        this.plansPagination = response.body.meta.pagination;
+                        return this.plans = response.body.data;
+                    },
+                    function (response) {
+                        console.log(response);
+                        return response.body.data;
+                    });
+            },
             getTeams(){
                 let params = {
-                    include: 'type',
+                    include: 'squads.members,type',
                     page: this.teamsPagination.current_page,
                 };
                 return this.$http.get('teams', { params: params }).then(function (response) {
@@ -214,10 +320,12 @@
                 }));
             }
 
+            promises.push(this.getPlans());
+            promises.push(this.getRoomTypes());
             promises.push(this.getTeams());
             Promise.all(promises).then(function (values) {
                 this.startUp = false;
-                this.searchReservations();
+//                this.searchReservations();
             }.bind(this));
         }
     }

b02642e3ec77d3b552eecc6833712b1ea75afa75
Neil Keena <nkeena1@gmail.com>
2017-05-05 16:57:23 -0400

    add rooming plan rooms endpoint

--- app/Http/Controllers/Api/Rooming/Plans/RoomsController.php
+++ app/Http/Controllers/Api/Rooming/Plans/RoomsController.php
@@ -6,6 +6,7 @@ use App\Http\Requests;
 use Illuminate\Http\Request;
 use App\Repositories\Rooming\Room;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\v1\RoomRequest;
 use App\Http\Transformers\v1\RoomTransformer;
 
 class RoomsController extends Controller
@@ -20,10 +21,38 @@ class RoomsController extends Controller
     public function index($planId, Request $request)
     {
         $rooms = $this->room
-                      ->inPlan($planId)
                       ->filter($request)
+                      ->inPlan($planId)
                       ->paginate($request->get('per_page'));
 
         return $this->response->paginator($rooms, new RoomTransformer);
+    }
+
+    public function show($planId, $id)
+    {
+        $room = $this->room->find($id)->get();
+
+        return $this->response->item($room, new RoomTransformer);
+    }
+
+    public function store($planId, RoomRequest $request)
+    {
+        $room = $this->room->make($request)->forPlan($planId)->get();
+
+        return $this->response->item($room, new RoomTransformer);
+    }
+
+    public function update($planId, $id, RoomRequest $request)
+    {
+        $room = $this->room->find($id)->modify($request)->get();
+
+        return $this->response->item($room, new RoomTransformer);
+    }
+
+    public function destroy($planId, $id)
+    {
+        $this->room->find($id)->delete();
+
+        return $this->response->noContent();
     }
 }

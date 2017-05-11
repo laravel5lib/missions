06a5880981005b185dadcb67a32ef60a800544f4
Jerez Bain <jerezb31@gmail.com>
2017-05-11 15:26:15 -0400

    fix duplicate roles issue #522

--- resources/assets/js/components/teams/team-manager.vue
+++ resources/assets/js/components/teams/team-manager.vue
@@ -1021,9 +1021,11 @@
             getRoles(search, loading){
                 loading ? loading(true) : void 0;
                 return this.$http.get('utilities/team-roles').then(function (response) {
+                    let roles = [];
                     _.each(response.body.roles, function (name, key) {
-                        this.rolesOptions.push({ value: key, name: name});
-                    }.bind(this));
+                        roles.push({ value: key, name: name});
+                    });
+                    this.rolesOptions = roles;
                     if (loading) {
                         loading(false);
                     } else {

84ce164dceff7a9274ec5fb4a6f54fc997cbdecf
Neil Keena <nkeena1@gmail.com>
2017-05-30 10:51:06 -0400

    add chevron toggle to rooming tutorial panel

--- resources/assets/js/components/rooms/rooming-manager.vue
+++ resources/assets/js/components/rooms/rooming-manager.vue
@@ -1,4 +1,40 @@
 <template>
+	<div>
+	<div class="row">
+	    <div class="col-sm-12">
+	        <div class="panel panel-default">
+	            <div class="panel-heading">
+	                <h6><a data-toggle="collapse" href="#collapseHints" aria-expanded="true" aria-controls="collapseHints">Rooming Tutorial <span class="pull-right"><i class="fa" :class="{ 'fa-chevron-up': toggleHintsCollapse, 'fa-chevron-down': !toggleHintsCollapse}"></i></span></a></h6>
+	            </div><!-- end panel-heading -->
+	            <div class="panel-body panel-collapse collapse in" id="collapseHints">
+	                <div class="row">
+	                    <div class="col-xs-12 col-sm-4">
+	                        <h5>Room management made simple</h5>
+	                        <p class="small">Utilize this simple tool to assign your missionaries to rooms.</p>
+	                    </div><!-- end col -->
+	                    <div class="col-xs-12 col-sm-8">
+	                        <div class="row">
+	                            <div class="col-xs-12">
+	                                <h5>Follow these simple steps</h5>
+	                            </div>
+	                            <div class="col-xs-12 col-sm-6">
+	                                <p class="small"><strong>Step 1</strong> Create your rooming plan.</p>
+	                                <p class="small"><strong>Step 2</strong> Add rooms to your rooming plan.</p>
+	                                <p class="small"><strong>Step 3</strong> Select your squad.</p>
+	                            </div><!-- end col -->
+	                            <div class="col-xs-12 col-sm-6">
+	                                <p class="small"><strong>Step 4</strong> Assign a Room Leader.</p>
+	                                <p class="small"><strong>Step 5</strong> Add Squad Members to rooms.</p>
+	                                <p class="small"><strong>Step 6</strong> Create more rooms as needed!</p>
+	                            </div><!-- end col -->
+	                        </div><!-- end row -->
+	                    </div><!-- end col -->
+	                </div><!-- end row -->
+	            </div><!-- end panel-body -->
+	        </div><!-- end panel -->
+	    </div><!-- end col -->
+	</div><!-- end row -->
+
 	<div class="row" style="position:relative;">
 		<spinner v-ref:spinner size="sm" text="Loading"></spinner>
 
@@ -475,6 +511,7 @@
 		</modal>
 
 	</div>
+	</div>
 </template>
 <style></style>
 <script type="text/javascript">
@@ -538,6 +575,7 @@
                     occupants: [],
                 },
                 storageName: 'TravelGroupRooming',
+                toggleHintsCollapse: true
             }
         },
 	    watch: {
@@ -853,6 +891,7 @@
             }
         },
         ready(){
+        	let self = this;
             let promises = [];
             if (this.isAdminRoute) {
 
@@ -895,6 +934,14 @@
             this.$root.$on('create-plan', function (val) {
                 this.openNewPlanModal();
             }.bind(this));
+
+            $('#collapseHints')
+	            .on('show.bs.collapse', function () {
+	                self.toggleHintsCollapse = true;
+	            })
+	            .on('hide.bs.collapse', function () {
+                    self.toggleHintsCollapse = false;
+                });
 
         }
     }

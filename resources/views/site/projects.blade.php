56cb2fd3fc50bcb047a87ef4532eeb27a07e80c0
zechnelson <hello@zechnelson.com>
2017-06-05 19:52:30 -0400

    Projects content adjustments

--- resources/views/site/projects.blade.php
+++ resources/views/site/projects.blade.php
@@ -14,12 +14,12 @@
 						<hr class="divider lg inv hidden-xs hidden-sm">
 						<h3 style="color:#ec6190;">Overwhelming Need</h3>
 						<img style="width:60px;margin-bottom:25px;" src="images/projects/dash.png" alt="">
-						<p>As one of the neediest nations in the Western Hemisphere, Nicaragua is overwhelmed with need for medical care, clean water and food. In cooperation with the local government, this global team will take on poverty and bring life-saving aid to those who need it.</p>
+						<p>As one of the neediest nations in the Western Hemisphere, Nicaragua is overwhelmed with opportunity for medical care, clean water and food. In cooperation with the local government, this global team will take on poverty and bring life-saving aid to those who need it.</p>
 					</div><!-- end col -->
 					<div class="col-xs-10 col-xs-offset-1 col-sm-6 col-md-4 text-center visible-xs">
 						<h3 style="color:#ec6190;">Overwhelming Need</h3>
 						<img style="width:60px;margin-bottom:25px;" src="images/projects/dash.png" alt="">
-						<p class="small">As one of the neediest nations in the Western Hemisphere, Nicaragua is overwhelmed with need for medical care, clean water and food. In cooperation with the local government, this global team will take on poverty and bring life-saving aid to those who need it.</p>
+						<p class="small">As one of the neediest nations in the Western Hemisphere, Nicaragua is overwhelmed with opportunity for medical care, clean water and food. In cooperation with the local government, this global team will take on poverty and bring life-saving aid to those who need it.</p>
 					<hr class="divider lg inv visible-xs">
 					</div><!-- end col -->
 					<div class="col-sm-6 col-md-offset-2">
@@ -53,7 +53,7 @@
 								</div><!-- end col -->
 							</div><!-- end row -->
 							<hr class="divider inv">
-							<p id="collapseFour" class="panel-collapse collapse small">Medical Clinics are open to the public and completely free. The Missions.Me medical teams will diagnose and prescribe free medicine to anyone who visits these clinics. We need to raise $25,000 to secure and ship the medicine needed for these clinics. To sponsor a single clinic is $7,500.</p>
+							<p id="collapseFour" class="panel-collapse collapse small">Our medical missionaries will come together to staff seven Medical Clinics across the nation offering medical diagnoses, treatment and the medicine required to treat hundreds of common conditions all free of charge. If your team would like to be partner with us to save lives in Nicaragua, $7,500 will purchase and ship the medicine and supplies for one clinic.</p>
 					<hr class="divider lg inv visible-xs">
 					</div><!-- end col -->
 					<div class="col-xs-10 col-xs-offset-1 col-sm-5 col-sm-offset-0">
@@ -76,7 +76,7 @@
 								</div><!-- end col -->
 							</div><!-- end row -->
 							<hr class="divider inv">
-							<p id="collapseThree" class="panel-collapse collapse small">Our team will be constructing 5 water filtration systems across the nation in communities that we've identified as places that do not have access to free clean water. The people there spend much of their income on buying clean water to drink, cook, etc.  These projects provide clean water to 1,000 people each week.</p>
+							<p id="collapseThree" class="panel-collapse collapse small">Currently there are impoverished communities who do not have access to free clean drinking water.  Thousands of people are either drinking contaminated water or spending a high percentage of their small wages on bottled water.  Our Clean Water Team Members will build water filtration systems in five of such communities.  The 500 gallon tanks will provide water for thousands of people in each community.  $7,000 will build a permanent system, including maintenance and training for local church leader who will manage the system after our team leaves.</p>
 					</div><!-- end col -->
 				</div><!-- end row -->
 				<hr class="divider lg inv">
@@ -101,32 +101,27 @@
 								</div><!-- end col -->
 							</div><!-- end row -->
 							<hr class="divider inv">
-							<p id="collapseOne" class="panel-collapse collapse small">The 1Nation1Day inititive is shipping several large containers loaded with meals for poverty stricken communities. Each container contains 270,000 meals and has a value of $159,000. The aid has been donated, all we need to do is ship it.  Shipping costs are $7,500 per container.</p>
+							<p id="collapseOne" class="panel-collapse collapse small">1Nation1Day has been blessed with a large donation of several shipping containers loaded with enough meals to feed 270,000 people. Each container is valued at over $159,000 in nutritious food.  Our ministry squads will distribute these meals to communities in Nicaragua that desperately need it. Since the containers have already been donated, your sponsorship of $7,500 will cover shipping and distribution costs.</p>
 					<hr class="divider lg inv visible-xs">
 					</div><!-- end col -->
 					<div class="col-xs-10 col-xs-offset-1 col-sm-5 col-sm-offset-0">
-							<div class="panel-featured" style="position:absolute;left:16px;top:-5px;z-index:999;">
-								<h6 class="btn btn-success btn-xs" style="border-radius:50px;"><i class="fa fa-usd icon-left"></i> 20k or 25k</h6>
-							</div>
 							<div class="hover-container">
 								<img class="hover-image img-responsive img-rounded" src="images/projects/leadership-thumb.jpg" alt="Leadership Conference">
 								<div class="hover-overlay" style="background-color: rgba(206, 255, 84, 1);">
-									<h4 class="hover-text text-center text-uppercase">Business & Leadership<br>Conferences</h4>
+									<h4 class="hover-text text-center text-uppercase">Start A Custom Fundraiser</h4>
 								</div>
 							</div>
 							<hr class="divider inv">
 							<div class="row">
 								<div class="col-xs-8">
-									<a class="btn btn-primary btn-sm" href="#">Donate Now</a>
+									<a class="btn btn-primary btn-sm" href="{{url('sponsor-a-project')}}">Start</a>
 								</div><!-- end col -->
 								<div class="col-xs-4">
 									<p style="margin-top:5px;"><a class="pull-right" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">Details <i class="fa fa-angle-down"></i></a></p>
 								</div><!-- end col -->
 							</div><!-- end row -->
 							<hr class="divider inv">
-							<div id="collapseTwo" class="panel-collapse collapse"><p class="small">The core piece of our plan for sustainable change in the nation of Nicaragua is education. During the week of 1Nation1Day we will be holding Leadership and Business conferences for the leaders around the nation of Nicaragua.</p>
-							<p class="small">Spiritual Leaders Conference<br><strong>$25,000</strong></p>
-							<p class="small">Business Conference<br><strong>$20,000</strong></p>
+							<div id="collapseTwo" class="panel-collapse collapse"><p class="small">Are you interested in setting up a custom fundraising page to help you raise funds for any of these projects? We'll hook you up. Learn more about how you can easily fundraise with Missions.Me.</p>
 							</div>
 					</div><!-- end col -->
 				</div><!-- end row -->

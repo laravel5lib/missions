dfc7a2670f58869de7553e5c680b4f4c17f43bf5
Neil Keena <nkeena1@gmail.com>
2017-06-14 19:15:08 -0400

    temporarily hide reports component

--- resources/assets/js/components/reservations/admin-reservations-list.vue
+++ resources/assets/js/components/reservations/admin-reservations-list.vue
@@ -319,7 +319,7 @@
 									:options="exportOptions"
 									:filters="exportFilters">
 					</export-utility>
-					<reservation-reports :filters="filters" :search="search"></reservation-reports>
+					<!-- <reservation-reports :filters="filters" :search="search"></reservation-reports> -->
                 </form>
             </div>
         </div>

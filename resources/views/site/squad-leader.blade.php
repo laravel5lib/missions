6ded1ead79072938b98771632eb7693fdb446d62
Neil Keena <nkeena1@gmail.com>
2017-06-21 14:43:06 -0400

    add blurb and squad leader manual download link

--- resources/views/site/squad-leader.blade.php
+++ resources/views/site/squad-leader.blade.php
@@ -13,7 +13,24 @@
 		<div class="row">
 			<div class="col-sm-8 col-sm-offset-2">
 				<h2 class="text-center">Squad Leader Training</h2>
+                <hr class="divider red-small">
+                <p class="text-center">Congratulations, you have been nominated to lead a squad in Nicaragua this summer. As an official "Squad Leader", you'll need to view the four-part training series with the Missions.Me Country Directors at your convenience.  All four parts will be viewable here.</p>
+                <p class="text-center"><strong>It is imperative that you view these sessions as well as read the Squad Leader Manual PDF document which can be downloaded below.</strong></p>
 				<hr class="divider inv">
+                <div class="row">
+                    <div class="col-xs-12 col-sm-10 col-sm-offset-1">
+                        <div class="panel panel-default">
+                            <div class="panel-body text-center">
+                                <h6 class="text-uppercase">
+                                    <a href="{{ download_file('resources/SLmanual2017_dig.pdf') }}">
+                                        <i class="fa fa-file-pdf-o fa-2x"></i> Download Squad Leader Manual
+                                        <small>(2.4MB)</small>
+                                    </a>
+                                </h6>
+                            </div><!-- end panel-body -->
+                        </div>
+                    </div>
+                </div>
 				<div class="row">
 					<div class="col-xs-12 col-sm-10 col-sm-offset-1">
 						<div class="panel panel-default">

1adc5ad762017aba403b6a57d9ec47edd3a97719
Neil Keena <nkeena1@gmail.com>
2017-07-03 07:31:07 -0700

    Update squad leader page; add rooming cost filter

--- resources/views/site/squad-leader.blade.php
+++ resources/views/site/squad-leader.blade.php
@@ -11,28 +11,27 @@
 	<div class="container">
 	<div class="content-section">
 		<div class="row">
-			<div class="col-sm-8 col-sm-offset-2">
+			<div class="col-sm-6 col-sm-offset-3">
 				<h2 class="text-center">Squad Leader Training</h2>
                 <hr class="divider red-small">
+                <hr class="divider inv sm">
                 <p class="text-center">Congratulations, you have been nominated to lead a squad in Nicaragua this summer. As an official "Squad Leader", you'll need to view the four-part training series with the Missions.Me Country Directors at your convenience.  All four parts will be viewable here.</p>
                 <p class="text-center"><strong>It is imperative that you view these sessions as well as read the Squad Leader Manual PDF document which can be downloaded below.</strong></p>
 				<hr class="divider inv">
                 <div class="row">
-                    <div class="col-xs-12 col-sm-10 col-sm-offset-1">
-                        <div class="panel panel-default">
-                            <div class="panel-body text-center">
-                                <h6 class="text-uppercase">
-                                    <a href="{{ download_file('resources/SLmanual2017_dig.pdf') }}">
-                                        <i class="fa fa-file-pdf-o fa-2x"></i> Download Squad Leader Manual
-                                        <small>(2.4MB)</small>
-                                    </a>
-                                </h6>
-                            </div><!-- end panel-body -->
-                        </div>
+                    <div class="col-xs-12 col-sm-10 col-sm-offset-1 text-center">
+                        <a class="btn btn-primary" href="{{ download_file('resources/SLmanual2017_dig.pdf') }}">
+                           	<i class="fa fa-file-pdf-o icon-left"></i> Squad Leader Manual
+                            <small>(2.4MB)</small>
+                        </a>
                     </div>
                 </div>
+                <hr class="divider inv lg">
+            </div><!-- end col -->
+        </div><!-- end row -->
+		<div class="row">
 				<div class="row">
-					<div class="col-xs-12 col-sm-10 col-sm-offset-1">
+					<div class="col-xs-12 col-sm-6">
 						<div class="panel panel-default">
 							<div class="video-outer">
 								<div class="video-inner">
@@ -41,6 +40,18 @@
 							</div><!-- end video-outer -->
 							<div class="panel-body text-center">
 								<h6 class="text-uppercase">Squad Leader &middot; Session 1</h6>
+							</div><!-- end panel-body -->
+						</div><!-- end panel -->
+					</div><!-- end col -->
+					<div class="col-xs-12 col-sm-6">
+						<div class="panel panel-default">
+							<div class="video-outer">
+								<div class="video-inner">
+									<iframe src="https://player.vimeo.com/video/222901667?title=0&byline=0&portrait=0" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+								</div><!-- end video-inner -->
+							</div><!-- end video-outer -->
+							<div class="panel-body text-center">
+								<h6 class="text-uppercase">Squad Leader &middot; Session 2</h6>
 							</div><!-- end panel-body -->
 						</div><!-- end panel -->
 					</div><!-- end col -->

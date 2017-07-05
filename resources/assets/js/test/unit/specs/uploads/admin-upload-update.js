abf4fc5f9c2a42e0c01a595d9abb66ebe1fbd0e6
Jerez Bain <jerezb31@gmail.com>
2017-07-04 23:28:18 -0400

    uploads unit tests complete checks Uploads group for #683

--- resources/assets/js/test/unit/specs/uploads/admin-upload-update.js
+++ resources/assets/js/test/unit/specs/uploads/admin-upload-update.js
@@ -6,7 +6,7 @@ import nextTick from "p-immediate";
 import test from "ava";
 
 //load the component with a vue instance
-RootInstance.template = '<div><upload-create-update is-update upload-id="90fd1c9a-78f1-4d2a-907d-31dbda79211e" type="avatar" name="avatar" ui-selector="2" v-ref:test-component></upload-create-update></div>';
+RootInstance.template = '<div><upload-create-update is-update upload-id="90fd1c9a-78f1-4d2a-907d-31dbda79211e" type="avatar" name="avatar" :ui-selector="2" is-child v-ref:test-component></upload-create-update></div>';
 RootInstance.components = {'upload-create-update': require('../../../../components/uploads/admin-upload-create-update.vue')};
 document.body.insertAdjacentHTML("afterbegin", "<app></app>");
 const vm = new Vue(RootInstance).$mount('app');
@@ -16,4 +16,23 @@ test('uploads populated', t => {
     t.notDeepEqual(AdminUploader.uploads, []);
 });
 
-test.todo('test update');
+test.before('set form data', t => {
+    AdminUploader.name = 'Upload Test';
+    AdminUploader.type = 'avatar';
+    AdminUploader.tags = ['User'];
+});
+
+test.todo('update upload - currently an issue with slimAPI code causing - Error: Maximum call stack size exceeded');
+/*
+test('update upload', async t => {
+    await nextTick();
+    let promise = AdminUploader.update();
+    if (!promise)  // if promise is a false boolean
+        t.fail();
+    promise.then(data => {
+        i.is(data.id, '90fd1c9a-78f1-4d2a-907d-31dbda79211e');
+        t.is(data.name, 'Upload Test');
+        t.is(data.type, 'avatar');
+        t.is(data.tags[0], 'User');
+    })
+});*/

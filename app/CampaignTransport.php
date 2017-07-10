0007e8e2795483192907199829e399cf9beaf01d
Neil Keena <nkeena1@gmail.com>
2017-07-10 14:50:14 -0700

    add new international transports endpoint
    `/campaigns/{id}/transports`

---
+++ app/CampaignTransport.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App;
+
+use App\Models\v1\Campaign;
+use App\Models\v1\Hub;
+use App\Models\v1\Passenger;
+use Carbon\Carbon;
+use EloquentFilter\Filterable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class CampaignTransport extends Model
+{
+    use UuidForKey, SoftDeletes, Filterable;
+
+    protected $table = 'transports';
+
+    protected $guarded = [];
+
+    protected $dates = ['created_at', 'updated_at', 'deleted_at', 'depart_at', 'arrive_at'];
+
+    public function campaign()
+    {
+        return $this->belongsTo(Campaign::class);
+    }
+
+    public function departureHub()
+    {
+        return $this->belongsTo(Hub::class, 'departure_hub_id');
+    }
+
+    public function arrivalHub()
+    {
+        return $this->belongsTo(Hub::class, 'arrival_hub_id');
+    }
+
+    public function passengers()
+    {
+        return $this->hasMany(Passenger::class);
+    }
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = strtolower(trim($value));
+    }
+
+    public function setCallSignAttribute($value)
+    {
+        if (! is_null($value)) {
+            $value = strtolower(trim($value));
+        }
+
+        $this->attributes['call_sign'] = $value;
+    }
+
+    public function setDepartAtAttribute($value)
+    {
+        if (! is_null($value)) {
+            $value = Carbon::parse($value);
+            // we make sure trailing seconds are 0
+            $value->setTime($value->format("H"), $value->format("i"), 0);
+        }
+
+        $this->attributes['depart_at'] = $value;
+    }
+
+    public function setArriveAtAttribute($value)
+    {
+        if (! is_null($value)) {
+            $value = Carbon::parse($value);
+            // we make sure trailing seconds are 0
+            $value->setTime($value->format("H"), $value->format("i"), 0);
+        }
+
+        $this->attributes['arrive_at'] = $value;
+    }
+}

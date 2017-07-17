f3c91768b2a278c36821e1dd044363a2fb2241b8
Jerez Bain <jerezb31@gmail.com>
2017-07-17 11:51:50 -0400

    - fixed transactions date issue - force moment filter to parse string in standard format from server-side

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -360,7 +360,7 @@ Vue.filter('moment', {
         }
 
         // console.log('before: ', val);
-        let date = moment.utc(val).local().format(format || 'LL');
+        let date = moment.utc(val, 'YYYY-MM-DD HH:mm:ss').local().format(format || 'LL');
 
         if (diff) {
             date = moment.utc(val).local().fromNow();
@@ -383,7 +383,7 @@ Vue.filter('moment', {
             return moment(val).format(format || 'LL'); // do not convert to local
         }
 
-        return moment(val).local().utc().format(format);
+        return moment(val, 'YYYY-MM-DD HH:mm:ss').local().utc().format(format);
     }
 });
 

99f6b5cccf758932f8fa74666df8c3767519e41c
Neil Keena <nkeena1@gmail.com>
2017-08-02 18:13:05 -0700

    sort by latest story

--- app/Http/Controllers/Api/StoriesController.php
+++ app/Http/Controllers/Api/StoriesController.php
@@ -35,6 +35,7 @@ class StoriesController extends Controller
     {
         $stories = $this->story
             ->filter($request->all())
+            ->latest()
             ->paginate($request->get('per_page', 10));
 
         return $this->response->paginator($stories, new StoryTransformer);

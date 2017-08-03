79413edaab83d036676221a2ccf771b769fc8ff3
Neil Keena <nkeena1@gmail.com>
2017-08-02 18:13:01 -0700

    fix profile story creation bug

--- resources/assets/js/components/users/user-profile-stories.vue
+++ resources/assets/js/components/users/user-profile-stories.vue
@@ -218,6 +218,7 @@
                 if(story) {
                     story.author_id = this.$root.user.id;
                     story.author_type = 'users';
+                    story.publications = [{ type: 'users', id: this.id }];
 
                     this.$http.post('stories', story).then(function (response) {
                         this.newMode = false;

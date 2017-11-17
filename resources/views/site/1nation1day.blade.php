396a3ca0b363357bc964ddd4c97788f0823f4771
zechnelson <hello@zechnelson.com>
2017-11-17 16:55:56 -0500

    story of 1n1d page

--- resources/views/site/1nation1day.blade.php
+++ resources/views/site/1nation1day.blade.php
@@ -1,111 +1,111 @@
 @extends('site.layouts.default')
-
+@push('styles') <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet"> @endpush
 @section('content')
-<div class="gray-lighter-bg">
+<div class="story-of-layout">
+<div class="gray-darker-bg main-header-bg">
 	<div class="container">
 	<div class="content-section">
 		<div class="row">
-			<div class="col-sm-8 col-sm-offset-2">
-				<h5>The Vision</h5>
-				<h2>To see entire nations changed in a day</h2>
+			<div class="col-sm-8 col-sm-offset-2 text-center">
+				<h5 class="text-uppercase text-primary">The Vision</h5>
+				<h1 class="text-oswald text-uppercase">To see entire nations changed in a day</h1>
 			</div><!-- end col -->
 		</div><!-- end row -->
 	</div>
 	</div><!-- end container -->
 </div><!-- end white-bg -->
-<div class="white-bg" style="border-top:4px solid #f6323e;">
-	<div class="container">
-		<div class="row">
-			<div class="col-sm-8 col-sm-offset-2">
-				<div style="margin-top:-150px;" class="video-outer">
-					<div class="video-inner">
-						<iframe src="https://player.vimeo.com/video/145551821" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-					</div>
-				</div>
-			</div><!-- end col -->
-		</div><!-- end row -->
-	</div><!-- end container -->
-</div><!-- end white-bg -->
-<div class="white-bg">
-	<div class="container">
-		<div class="row">
-			<div class="col-sm-6" style="border-top:4px solid #f6323e;">
-				<h1 class="text-primary">2,502,389</h1>
-				<h6>Total People Reached</h6>
-			</div><!-- end col -->
-			<div class="col-sm-6">
-				<img src="#" />
-			</div><!-- end col -->
-		</div><!-- end row -->
-	</div><!-- end container -->
-	<h1>1Nation1Day</h1>
-</div><!-- end white-bg -->
-<div class="gray-lighter-bg">
+<div class="gray-light-bg stadium-light-bg" style="border-top:4px solid #f6323e;">
 	<div class="container">
 	<div class="content-section">
 		<div class="row">
-			<div class="col-sm-6 col-sm-offset-3">
-				<h6 class="text-uppercase">Missionaries Sent</h6>
-				<p>Whether you donated towards sending a missionary or you were a missionary, the reach was huge! This is truly the dawn of a new missions era.</p>
-			</div><!-- end col -->
-		</div><!-- end row -->
-		<div class="row">
-			<div class="col-sm-6">
-				<div class="video-outer">
+			<div class="col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">
+				<div class="video-outer story-vid">
 					<div class="video-inner">
-						<iframe src="https://player.vimeo.com/video/145551821" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+						<iframe src="https://player.vimeo.com/video/241925833?title=0&byline=0&portrait=0" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
 					</div>
 				</div>
 			</div><!-- end col -->
-			<div class="col-sm-6">
-				<div class="panel panel-default">
-					<div class="panel-body">
-						<h4>748,778</h4>
-						<p>Total students reached through our ministry teams</p>
-					</div><!-- end panel-body -->
-				</div><!-- end panel -->
-				<div class="panel panel-default">
-					<div class="panel-body">
-						<h4>8,370</h4>
-						<p>Total missionaries sent</p>
-					</div><!-- end panel-body -->
-				</div><!-- end panel -->
-				<h6>Nearly 90 people reached per missionary</h6>
+		</div><!-- end row -->
+		<hr class="divider inv lg">
+		<hr class="divider inv lg">
+		<hr class="divider inv lg">
+	</div><!-- end content-section -->
+	</div><!-- end container -->
+</div><!-- end white-bg -->
+<div class="white-bg topographic-bg">
+	<div class="container">
+		<div class="content-section">
+			<div class="row">
+				<div class="col-sm-6 col-sm-offset-3 text-center">
+					<h1 class="text-primary text-oswald font-hero">2,502,389</h1>
+					<h5 class="text-uppercase">Total People Reached</h5>
+					<hr class="divider inv lg">
+					<img class="custom-arrow" src="../images/storyof/custom-down-arrow.png" />
+				</div><!-- end col -->
+			</div><!-- end row -->
+		</div><!-- end content-section -->
+	</div><!-- end container -->
+	<!-- <h1 class="text-center text-uppercase text-primary font-bg">1Nation1Day</h1> -->
+</div><!-- end white-bg -->
+<div class="white-bg topographic-bg">
+	<div class="container">
+	<div class="content-section">
+		<div class="row">
+			<div class="col-sm-6 col-sm-offset-3 text-center">
+				<h5 class="text-uppercase">Missionaries Sent</h5>
+				<p>Whether you donated towards sending a missionary or you were a missionary, the reach was huge! This is truly the dawn of a new missions era.</p>
+				<hr class="divider inv lg">
+				<hr class="divider inv lg">
+			</div><!-- end col -->
+		</div><!-- end row -->
+		<div class="row">
+			<div class="col-sm-7">
+				<div class="video-outer">
+					<div class="video-inner">
+						<iframe src="https://player.vimeo.com/video/224954342?title=0&byline=0&portrait=0" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+					</div>
+				</div>
+			</div><!-- end col -->
+			<div class="col-sm-4 col-sm-offset-1">
+				<h1 class="text-oswald text-primary text-uppercase">748,778</h1>
+				<hr class="divider red-small">
+				<p>Total students reached through our ministry teams</p>
+				<h1 class="text-oswald text-primary text-uppercase">8,370</h1>
+				<hr class="divider red-small">
+				<p>Total missionaries sent</p>
+				<hr class="divider inv lg">
+				<h6>Nearly <span class="text-primary">90</span> people reached per missionary</h6>
 			</div><!-- end col -->
 		</div><!-- end row -->
 	</div>
 	</div><!-- end container -->
 </div><!-- end gray-lighter-bg -->
-<div class="white-bg">
+<div class="white-bg" style="border-top:1px solid #e6e6e6;">
 	<div class="container">
 	<div class="content-section">
 		<div class="row">
-			<div class="col-sm-6 col-sm-offset-3">
-				<h6 class="text-uppercase">Stadium Outreaches</h6>
+			<div class="col-sm-6 col-sm-offset-3 text-center">
+				<h5 class="text-uppercase">Stadium Outreaches</h5>
 				<p>A time for an entire country to join in the largest stadiums across the nation to proclaim change under the name of Jesus.</p>
+				<hr class="divider inv lg">
+				<hr class="divider inv lg">
 			</div><!-- end col -->
 		</div><!-- end row -->
 		<div class="row">
-			<div class="col-sm-6">
+			<div class="col-sm-4 col-sm-offset-1">
+				<h1 class="text-oswald text-primary text-uppercase">1,148,500</h1>
+				<hr class="divider red-small">
+				<p>Total people reached through stadium outreaches</p>
+				<h1 class="text-oswald text-primary text-uppercase">52</h1>
+				<hr class="divider red-small">
+				<p>Total stadium outreaches</p>
+			</div><!-- end col -->
+			<div class="col-sm-7">
 				<div class="video-outer">
 					<div class="video-inner">
-						<iframe src="https://player.vimeo.com/video/145551821" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+						<iframe src="https://player.vimeo.com/video/228425744?title=0&byline=0&portrait=0" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
 					</div>
 				</div>
-			</div><!-- end col -->
-			<div class="col-sm-6">
-				<div class="panel panel-default">
-					<div class="panel-body">
-						<h4>1,148,500</h4>
-						<p>Total people reached through stadium outreaches</p>
-					</div><!-- end panel-body -->
-				</div><!-- end panel -->
-				<div class="panel panel-default">
-					<div class="panel-body">
-						<h4>52</h4>
-						<p>Total stadium outreaches</p>
-					</div><!-- end panel-body -->
-				</div><!-- end panel -->
 			</div><!-- end col -->
 		</div><!-- end row -->
 	</div>
@@ -115,88 +115,85 @@
 	<div class="container">
 	<div class="content-section">
 		<div class="row">
-			<div class="col-sm-6 col-sm-offset-3">
-				<h6 class="text-uppercase">Healing Nations</h6>
+			<div class="col-sm-6 col-sm-offset-3 text-center">
+				<h5 class="text-uppercase">Healing Nations</h5>
 				<p>We believe that in order to open the door to a nations heart you must reach the practical needs of the nation. Here’s how, together, we healed nations.</p>
+				<hr class="divider inv lg">
+				<hr class="divider inv lg">
 			</div><!-- end col -->
 		</div><!-- end row -->
 		<div class="row">
-			<div class="col-sm-6">
+			<div class="col-sm-7">
 				<div class="video-outer">
 					<div class="video-inner">
-						<iframe src="https://player.vimeo.com/video/145551821" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+						<iframe src="https://player.vimeo.com/video/132365635?title=0&byline=0&portrait=0" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
 					</div>
 				</div>
 			</div><!-- end col -->
-			<div class="col-sm-6">
-				<div class="panel panel-default">
-					<div class="panel-body">
-						<h4>44,198</h4>
-						<p>Total people reached at medical clinics</p>
-					</div><!-- end panel-body -->
-				</div><!-- end panel -->
-				<div class="panel panel-default">
-					<div class="panel-body">
-						<h4>545,729</h4>
-						<p>Total people provided humanitarian aid</p>
-					</div><!-- end panel-body -->
-				</div><!-- end panel -->
+			<div class="col-sm-4 col-sm-offset-1">
+				<h1 class="text-oswald text-primary text-uppercase">44,198</h1>
+				<hr class="divider red-small">
+				<p>Total people reached at medical clinics</p>
+				<h1 class="text-oswald text-primary text-uppercase">545,729</h1>
+				<hr class="divider red-small">
+				<p>Total people provided humanitarian aid</p>
+				<hr class="divider inv lg">
 				<h6>Aid includes shoe & food distribution, home builds and clean water filtration systems.</h6>
 			</div><!-- end col -->
 		</div><!-- end row -->
 	</div>
 	</div><!-- end container -->
 </div><!-- end gray-lighter-bg -->
-<div class="white-bg">
+<div class="white-bg topographic-bg" style="border-bottom:4px solid #004561;">
 	<div class="container">
 	<div class="content-section">
 		<div class="row">
-			<div class="col-sm-6 col-sm-offset-3">
-				<h6 class="text-uppercase">Leaders Equipped</h6>
+			<div class="col-sm-6 col-sm-offset-3 text-center">
+				<h5 class="text-uppercase">Leaders Equipped</h5>
 				<p>We believe that in order to open the door to a nations heart you must reach the practical needs of the nation. Here’s how, together, we healed nations.</p>
+				<hr class="divider inv lg">
+				<hr class="divider inv lg">
 			</div><!-- end col -->
 		</div><!-- end row -->
 		<div class="row">
-			<div class="col-sm-6">
+			<div class="col-sm-4 col-sm-offset-1">
+				<h1 class="text-oswald text-primary text-uppercase">42,070</h1>
+				<hr class="divider red-small">
+				<p>Total pastors, business and political leaders equipped</p>
+				<h1 class="text-oswald text-primary text-uppercase">607</h1>
+				<hr class="divider red-small">
+				<p>Missions.Me Team Leaders activated and released</p>
+			</div><!-- end col -->
+			<div class="col-sm-7">
 				<div class="video-outer">
 					<div class="video-inner">
-						<iframe src="https://player.vimeo.com/video/145551821" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+						<iframe src="https://player.vimeo.com/video/227029480?title=0&byline=0&portrait=0" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
 					</div>
 				</div>
-			</div><!-- end col -->
-			<div class="col-sm-6">
-				<div class="panel panel-default">
-					<div class="panel-body">
-						<h4>42,070</h4>
-						<p>Total pastors, business and political leaders equipped</p>
-					</div><!-- end panel-body -->
-				</div><!-- end panel -->
-				<div class="panel panel-default">
-					<div class="panel-body">
-						<h4>607</h4>
-						<p>Missions.Me Team Leaders activated and released</p>
-					</div><!-- end panel-body -->
-				</div><!-- end panel -->
 			</div><!-- end col -->
 		</div><!-- end row -->
 	</div>
 	</div><!-- end container -->
 </div><!-- end white-bg -->
-<div class="gray-lighter-bg">
+<div class="gray-lighter-bg cta-stadium-bg">
 	<div class="container">
 	<div class="content-section">
 		<div class="row">
-			<div class="col-sm-8 col-sm-offset-2">
-				<h1 class="text-uppercase">Join us on a life changing trip this year!</h1>
+			<div class="col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+				<h1 class="text-center text-uppercase text-oswald">Let's change nations together!</h1>
+				<h4 class="text-center">How can you get involved?</h4>
+				<hr class="divider inv lg">
 			</div><!-- end col -->
-			<div class="col-sm-4 col-sm-offset-2">
-				<a class="btn btn-primary btn-lg btn-block" href="#">Go On A Trip</a>
+			<div class="col-xs-10 col-xs-offset-1 col-sm-4 col-sm-offset-2 col-md-3 col-md-offset-3">
+					<a class="btn btn-primary btn-md btn-block" href="{{ url('campaigns') }}">Go On A Trip</a>
+					<hr class="divider inv md">
 			</div><!-- end col -->
-			<div class="col-sm-4">
-				<a class="btn btn-primary btn-lg btn-block" href="#">Donate</a>
+			<div class="col-xs-10 col-xs-offset-1 col-sm-4 col-md-3 col-sm-offset-0 col-md-offset-0">
+					<a class="btn btn-primary btn-md btn-block" href="{{ url('donate') }}">Give Towards 1N1D</a>
 			</div><!-- end col -->
 		</div><!-- end row -->
 	</div>
 	</div><!-- end container -->
 </div><!-- end gray-lighter-bg -->
+</div><!-- end story of layout -->
 @endsection

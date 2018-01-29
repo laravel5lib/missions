00d7b869e7895456049706b36139459231923205
Neil Keena <nkeena1@gmail.com>
2018-01-29 15:57:25 -0800

    closes #862, update dashboard featured news

--- resources/views/dashboard/_featurednews.blade.php
+++ resources/views/dashboard/_featurednews.blade.php
@@ -5,15 +5,12 @@
     <div class="panel-body" style="height:249px;">
         <div class="row">
             <div class="col-xs-12 text-center" style="padding:20px;">
-                <h5>Join us for the historic</h5>
-                <h3>1Nation1Day 2017</h3>
-                <h5>Nicaragua</h5>
+                <h5>Join us this Summer for</h5>
+                <h3>Advance 2018</h3>
+                <h5>3 Nations 1 Generation</h5>
                 <hr class="divider inv">
-                <a class="btn btn-primary" href="#">Join Us</a>
+                <a class="btn btn-primary" href="/campaigns">Learn More</a>
             </div>
         </div>
     </div>
-    {{-- <div class="panel-footer text-center" style="padding:10px;">
-        <a class="small" style="color:#bcbcbc;" href="/campaigns">View Other Campaigns</a>
-    </div> --}}
 </div>

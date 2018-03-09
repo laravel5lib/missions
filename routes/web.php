0aeef1ca2692615fe51272b70c7f1a35474ff8e8
Neil Keena <nkeena1@gmail.com>
2018-03-09 10:37:36 -0800

    Group requests (#901)
    * - fix vue-strap issue
    
    * datepicker fixed
    
    * HOTFIX: transactions form errors and sibling components updating #794
    
    * #797 Fix form errors after successful submission
    - group interest
    - trip interest
    - speaker request
    - contact us
    - sponsor project
    
    * #799 group filter functions properly
    
    * make array that holds records of messages reactive for each field
    
    * basic info - computed prop issue fixed
    
    * add facebook pixel to global header
    
    * merge
    
    * document manager event issues - event watcher typo
    
    * reservation edit form issues fixed
    
    * age filter fixed, closes #823
    
    * closes #824
    
    * closes #826
    fixes weird phone-input issue
    
    * oops
    
    * oops :P
    
    * oops :/
    
    * closes #827
    
    * closes #828
    
    * bump
    
    * amount values filtered did not match results
    
    * closes #831 group edit form populating properly
    
    * closes #831 - Created At info added to template
    
    * fix additional options bug in trip reg
    
    * pre-populate user data
    
    * closes #852 promoValid typo and formatting
    
    * #871 - simplify search debounce
    
    * closes #863 - this was `undefined` needed arrow function
    
    * clean `package.json`
    
    * merge
    
    * global spinners working again
    
    * #895 group trip moved to `/teams/request`. You may want more UI changes though
    
    * "#895 group trip moved to `/teams/request`. You may want more UI changes though"
    
    This reverts commit c7e9f4b420c188d8891e8191636886ed6d0300c1.
    
    * Revert ""#895 group trip moved to `/teams/request`. You may want more UI changes though""
    
    This reverts commit 28f3959e00bee2e59252e3921867a52a087a62cc.

--- routes/web.php
+++ routes/web.php
@@ -129,6 +129,7 @@ $this->get('/groups', function () {
     return redirect('/teams');
 });
 $this->get('/teams', 'GroupsController@index');
+$this->get('/teams/request', 'GroupsController@request');
 $this->get('/{slug}/signup', 'GroupsController@signup')
      ->where('sponsor_slug', '^(?!api).*$');
 $this->get('/{slug}/trips', 'CampaignsController@trips')

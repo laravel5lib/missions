990fdf064ef5623d442cbe3a7fd649bdad11b449
zechnelson <hello@zechnelson.com>
2018-03-29 16:44:26 -0400

    Coordinators page

---
+++ resources/views/site/coordinators.blade.php
@@ -0,0 +1,140 @@
+@extends('site.layouts.default')
+
+@section('title', 'Coordinators - Missions.Me')
+
+@section('content')
+<div class="content-page-header">
+    <img class="img-responsive" src="images/coordinators/coordinators-header.jpg" alt="Coordinators">
+    <div class="c-page-header-text">
+    	<h1 class="text-uppercase dash-trailing">Coordinators</h1>
+    </div><!-- end c-page-header-content -->
+</div><!-- end c-page-header -->
+    <div class="gray-lighter-bg">
+      <div class="container">
+        <div class="content-section">
+          <div class="row">
+            <div class="col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1 text-center">
+              <h6 class="text-uppercase">We’re here for you</h6>
+              <h2>Here To Serve You!</h2>
+              <hr class="divider red-small xlg">
+            </div><!-- end col -->
+          </div><!-- end row -->
+          <hr class="divider inv xlg">
+          <div class="row">
+            <div class="col-xs-12 text-center">
+              <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdQw1z2ZvpuIAW4Xm4gsU35s7tue8-cMbPANrvs-t6386syAA/viewform?embedded=true" width="1050" height="1000" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+            </div><!-- end col -->
+          </div><!-- end row -->
+        </div><!-- end content-section -->
+      </div><!-- end container -->
+    </div><!-- end gray-lighter-bg -->
+    <!-- <div class="gray-light-bg">
+      <div class="container">
+        <div class="content-section">
+          <div class="row">
+            <div class="col-xs-10 col-xs-offset-1">
+              <h2 class="text-center">Launch Your Team</h2>
+              <h5 class="text-center">Everything you need for an incredible team launch!</h5>
+              <hr class="divider red-small xlg">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div> -->
+    <!-- <div class="gray-lighter-bg">
+      <div class="container">
+        <div class="content-section">
+          <div class="row">
+            <div class="col-xs-10 col-xs-offset-1">
+              <h2 class="text-center">Monthly Campaign Updates</h2>
+              <hr class="divider red-small xlg">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div> -->
+    <!-- <div class="gray-light-bg">
+      <div class="container">
+        <div class="content-section">
+          <div class="row">
+            <div class="col-xs-10 col-xs-offset-1">
+              <h2 class="text-center">Monthly Coordinator Calls</h2>
+              <hr class="divider red-small xlg">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div> -->
+    <div class="gray-lighter-bg">
+      <div class="container">
+        <div class="content-section">
+          <div class="row">
+            <div class="col-xs-10 col-xs-offset-1">
+              <h2 class="text-center">Who We Are</h2>
+              <hr class="divider red-small xlg">
+              <div class="row">
+                <div class="col-md-3 col-md-offset-0 col-sm-6 col-sm-offset-0 col-xs-10 col-xs-offset-1">
+                  <div class="panel">
+                    <a href="http://stripe.com" target="_blank">
+                      <img class="img-responsive" src="images/coordinators/matt.jpg" alt="">
+                    </a>
+                    <div class="panel-body">
+                      <h5>Matt Sheehan</h5>
+                      <p style="margin-bottom:5px;" class="small">Director Of Operations</p>
+                      <p style="margin-bottom:0;"><a href="http://instagram.com/_sheehan"><i style="vertical-align:middle;" class="fa fa-instagram"></i></a></p>
+                    </div>
+                  </div>
+                </div><!-- end col -->
+                <div class="col-md-3 col-md-offset-0 col-sm-6 col-sm-offset-0 col-xs-10 col-xs-offset-1">
+                  <div class="panel">
+                      <img class="img-responsive" src="images/coordinators/jontel.jpg" alt="">
+                    <div class="panel-body">
+                      <h5>Jontel Lidman</h5>
+                      <p style="margin-bottom:5px;" class="small">Trip Operations</p>
+                      <p style="margin-bottom:0;"><a href="http://instagram.com/jontel08"><i style="vertical-align:middle;" class="fa fa-instagram"></i></a></p>
+                    </div>
+                  </div>
+                </div><!-- end col -->
+                <div class="col-md-3 col-md-offset-0 col-sm-6 col-sm-offset-0 col-xs-10 col-xs-offset-1">
+                  <div class="panel panel-default">
+                      <img class="img-responsive" src="images/coordinators/lib.jpg" alt="">
+                    <div class="panel-body">
+                      <h5>Libby Gonzales</h5>
+                      <p style="margin-bottom:5px;" class="small">Team Relations</p>
+                      <p style="margin-bottom:0;"><a href="http://instagram.com/Libbygonzales"><i style="vertical-align:middle;" class="fa fa-instagram"></i></a></p>
+                    </div>
+                  </div>
+                </div><!-- end col -->
+                <div class="col-md-3 col-md-offset-0 col-sm-6 col-sm-offset-0 col-xs-10 col-xs-offset-1">
+                  <div class="panel">
+                      <img class="img-responsive" src="images/coordinators/jackie.jpg" alt="">
+                    <div class="panel-body">
+                      <h5>Jackie Rios</h5>
+                      <p style="margin-bottom:5px;" class="small">Accounts</p>
+                      <p style="margin-bottom:0;"><a href="http://instagram.com/jackiechackie"><i style="vertical-align:middle;" class="fa fa-instagram"></i></a></p>
+                    </div><!-- end panel-body -->
+                  </div><!-- end panel -->
+                </div><!-- end col -->
+              </div><!-- end row -->
+            </div><!-- end col -->
+          </div><!-- end row -->
+        </div><!-- end content-section -->
+      </div><!-- end container -->
+    </div><!-- end gray-lighter-bg -->
+    <div class="gray-light-bg">
+      <div class="container">
+        <div class="content-section">
+          <div class="row">
+            <div class="col-sm-6 col-sm-offset-0 col-xs-12 text-center">
+              <h3>Questions Or Comments?</h3>
+              <a class="btn btn-primary btn-md" href="mailto:coordinators@missions.me">Email Us</a>
+            </div><!-- end col -->
+            <div class="col-sm-6 col-sm-offset-0 col-xs-12 text-center">
+              <h3>Questions about missionary accounts?</h3>
+              <a class="btn btn-primary btn-md" href="mailto:accounts@missions.me">Email Us</a>
+            </div><!-- end col -->
+          </div><!-- end row -->
+        </div><!-- end content-section -->
+      </div><!-- end container -->
+    </div><!-- end gray-lighter-bg -->
+@endsection

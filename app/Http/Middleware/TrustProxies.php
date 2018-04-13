51f28e6d10f3f5ef503b6c96d13f2ebe914d4761
Neil Keena <nkeena1@gmail.com>
2018-04-12 22:01:28 -0700

    configured trusted proxy

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array
      */
-    protected $proxies;
+    protected $proxies = ['10.134.31.80'];
 
     /**
      * The headers that should be used to detect proxies.

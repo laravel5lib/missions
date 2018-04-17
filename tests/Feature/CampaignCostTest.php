f99cae6491f310405f8568417e903ba5eaa71754
Neil Keena <nkeena1@gmail.com>
2018-04-17 10:35:40 -0700

    failing test, update with json assertion

--- tests/Feature/CampaignCostTest.php
+++ tests/Feature/CampaignCostTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
+use App\Models\v1\Cost;
+use App\Models\v1\Trip;
 use App\Models\v1\Campaign;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -14,10 +16,26 @@ class CampaignCostTest extends TestCase
     /** @test */
     public function get_all_costs_for_a_campaign()
     {
+        // create campaign with cost
         $campaign = factory(Campaign::class)->create();
+        factory(Cost::class)->create(['cost_assignable_id' => $campaign->id, 'cost_assignable_type' => 'campaigns']);
         
+        // create trip with cost
+        $trip = factory(Trip::class)->create(['campaign_id' => $campaign->id]);
+        factory(Cost::class)->create(['cost_assignable_id' => $trip->id, 'cost_assignable_type' => 'trips']);
+        
+        // get only the campaign's costs
         $response = $this->json('GET', "/api/campaigns/{{ $campaign->id }}/costs");
 
         $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'data' => [
+                [
+                    'id', 'name', 'amount', 'description', 'type', 'active_at'
+                ]
+            ],
+            'meta'
+        ]);
+        $response->assertJson(['meta' => ['total' => 1]]);
     }
 }

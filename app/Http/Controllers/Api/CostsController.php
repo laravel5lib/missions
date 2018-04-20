26d8a0b3d245ef21e2435a34d6f21b9ecb9219fd
Neil Keena <nkeena1@gmail.com>
2018-04-20 16:33:44 -0700

    add index method to make test pass

--- app/Http/Controllers/Api/CostsController.php
+++ app/Http/Controllers/Api/CostsController.php
@@ -5,10 +5,18 @@ namespace App\Http\Controllers\api;
 use App\Models\v1\Cost;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\CostResource;
 use App\Http\Requests\v1\CostRequest;
 
 class CostsController extends Controller
 {
+    public function index()
+    {
+        $costs = Cost::paginate();
+
+        return CostResource::collection($costs);
+    }
+    
     public function store(CostRequest $request)
     {
         Cost::create($request->all());
--- tests/Feature/CostTest.php
+++ tests/Feature/CostTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
+use App\Models\v1\Cost;
 use App\Models\v1\User;
 use Laravel\Passport\Passport;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -28,6 +29,8 @@ class CostTest extends TestCase
     public function gets_all_costs()
     {
         Passport::actingAs(factory(User::class)->create());
+        
+        factory(Cost::class, 2)->create();
 
         $response = $this->json('get', '/api/costs');
 

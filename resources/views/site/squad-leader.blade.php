fc7b02bcbda862bd801493db24c4ec2c2774f95e
Neil Keena <nkeena1@gmail.com>
2018-07-17 16:30:30 -0700

    add session 4 video

--- resources/views/site/squad-leader.blade.php
+++ resources/views/site/squad-leader.blade.php
@@ -98,18 +98,11 @@
                     </div><!-- end col -->
 					<div class="col-xs-12 col-sm-6">
 						<div class="panel panel-default">
-							<div>
-								<div style="background: #f5f5f5;">
-									<div style="min-height: 315px; display: flex; align-items: center;">
-										<div class="text-center text-muted" style="padding: 5px">
-											<h2>LIVE ON ZOOM</h2>
-											<h5>Monday July 16 at 9pm eastern</h5>
-											<p>Link to Join Call: <a href="https://zoom.us/j/155334666">https://zoom.us/j/155334666</a></p>
-											<p>Be sure to attend to get last minute details and ask questions about being a Squad Leader!</p>
-										</div>
-									</div>
-								</div><!-- end video-inner -->
-							</div><!-- end video-outer -->
+							<div class="video-outer">
+                                <div class="video-inner">
+                                    <iframe src="https://player.vimeo.com/video/280297722?title=0&byline=0&portrait=0" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+                                </div><!-- end video-inner -->
+                            </div><!-- end video-outer -->
 							<div class="panel-body text-center">
 								<h6 class="text-uppercase">Squad Leader &middot; Session 4</h6>
 							</div><!-- end panel-body -->

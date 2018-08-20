e0d94b0fdef279df78f37b6ee9e7ff4579c6ddfa
Neil Keena <nkeena1@gmail.com>
2018-08-20 10:07:31 -0700

    fix duplicate notifications

--- app/Jobs/Reservations/ProcessReservation.php
+++ app/Jobs/Reservations/ProcessReservation.php
@@ -2,7 +2,6 @@
 
 namespace App\Jobs\Reservations;
 
-use App\Events\ReservationWasProcessed;
 use App\Jobs\Job;
 use App\Models\v1\Reservation;
 use Illuminate\Http\Request;
@@ -41,7 +40,5 @@ class ProcessReservation extends Job
     public function handle()
     {
         $this->reservation->process($this->costs);
-
-        event(new ReservationWasProcessed($this->reservation));
     }
 }

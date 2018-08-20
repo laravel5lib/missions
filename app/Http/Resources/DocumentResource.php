aa14913aed2522ddcc6e847ee2c0e08e73680649
Neil Keena <nkeena1@gmail.com>
2018-08-20 12:28:43 -0700

    refactor document resource object

--- app/Http/Resources/DocumentResource.php
+++ app/Http/Resources/DocumentResource.php
@@ -20,77 +20,27 @@ class DocumentResource extends JsonResource
     private function getFields($type) {
         switch ($type) {
             case 'passports':
-                return [
-                    'id' => $this->id,
-                    'given_names' => $this->given_names,
-                    'surname' => $this->surname,
-                    'number' => $this->number,
-                    'birth_country' => $this->birth_country,
-                    'birth_country_name' => country($this->birth_country),
-                    'citizenship' => $this->citizenship,
-                    'citizenship_name' => country($this->citizenship),
-                    'upload_id' => $this->upload_id,
-                    'expires_at' => $this->expires_at->format('Y-m-d'),
-                    'created_at' => $this->created_at->toIso8601String(),
-                    'updated_at' => $this->updated_at->toIso8601String(),
-                    'expired' => $this->expires_at->isPast() ? true : false,
-                ];
+                return new PassportResource($this);
                 break;
 
             case 'visas':
-                return [
-                    'id' => $this->id,
-                    'given_names' => $this->given_names,
-                    'surname' => $this->surname,
-                    'number' => $this->number,
-                    'country' => $this->country_code,
-                    'country_name' => country($this->country_code),
-                    'upload_id' => $this->upload_id,
-                    'issued_at' => $this->issued_at->format('Y-m-d'),
-                    'expires_at' => $this->expires_at->format('Y-m-d'),
-                    'created_at' => $this->created_at->toIso8601String(),
-                    'updated_at' => $this->updated_at->toIso8601String(),
-                    'expired' => $this->expires_at->isPast() ? true : false,
-                ];
+                return new VisaResource($this);
                 break;
             
             case 'essays':
-                return [
-                    'id' => $this->id,
-                    'author_name' => $this->author_name,
-                    'subject' => $this->subject,
-                    'content' => $this->content,
-                    'created_at' => $this->created_at->toIso8601String(),
-                    'updated_at' => $this->updated_at->toIso8601String()
-                ];
+                return new EssayResource($this);
                 break;
 
             case 'influencer-applications':
-                return [
-                    'id' => $this->id,
-                    'author_name' => $this->author_name,
-                    'subject' => $this->subject,
-                    'content' => $this->content,
-                    'created_at' => $this->created_at->toIso8601String(),
-                    'updated_at' => $this->updated_at->toIso8601String()
-                ];
+                return new EssayResource($this);
                 break;
 
             case 'referrals':
-                return [
-                    'id' => $this->id,
-                    'applicant_name' => $this->applicant_name,
-                    'type' => $this->type,
-                    'attention_to' => $this->attention_to,
-                    'recipient_email' => $this->recipient_email,
-                    'referrer' => $this->referrer,
-                    'response' => $this->response,
-                    'status' => $this->status,
-                    'sent_at' => $this->sent_at ? $this->sent_at->toIso8601String() : null,
-                    'responded_at' => $this->responded_at ? $this->responded_at->toIso8601String() : null,
-                    'created_at' => $this->created_at->toIso8601String(),
-                    'updated_at' => $this->updated_at->toIso8601String(),
-                ];
+                return new ReferralResource($this);
+                break;
+
+            case 'medical-releases':
+                return new MedicalReleaseResource($this);
                 break;
 
             default:

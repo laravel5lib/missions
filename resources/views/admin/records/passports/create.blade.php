c8884df47a0ea22104fbcef4ec749c3a25574dca
Neil Keena <nkeena1@gmail.com>
2018-08-24 11:43:24 -0700

    update passport form views

--- resources/views/admin/records/passports/create.blade.php
+++ resources/views/admin/records/passports/create.blade.php
@@ -8,29 +8,22 @@
 @endsection
 
 @section('content')
-    <div class="white-header-bg">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-8">
-                    <h3 class="hidden-xs">User Passports <small>&middot; Create</small></h3>
-                    <h3 class="visible-xs text-center">User Passports<br><small>Create</small></h3>
-                </div>
-                <div class="col-sm-4 text-right hidden-xs">
-                    <hr class="divider inv sm">
-                    <a onclick="window.history.back()" class="btn btn-primary"><i class="fa fa-chevron-left icon-left"></i> Back</a>
-                </div>
-                <div class="col-sm-4 text-center visible-xs">
-                    <a onclick="window.history.back()" class="btn btn-primary"><i class="fa fa-chevron-left icon-left"></i> Back</a>
-                    <hr class="divider inv sm">
-                </div>
-            </div>
-        </div>
-    </div>
+
+    @breadcrumbs(['links' => [
+        'admin' => 'Dashboard',
+        'admin/records/passports' => 'Travel Documents',
+        'active' => 'Passports'
+    ]])
+    @endbreadcrumbs
+
     <hr class="divider inv lg">
     <div class="container">
         <div class="row">
             <div class="col-sm-8 col-sm-offset-2">
                 <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h5>Passport</h5>
+                    </div>
                     <div class="panel-body">
                         <passport-create-update 
                             :for-admin="true" 

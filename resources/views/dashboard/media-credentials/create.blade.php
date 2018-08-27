068bd2e494793f1ce35e5eba2cff210434278d6c
Neil Keena <nkeena1@gmail.com>
2018-08-27 14:53:39 -0700

    update more media credentials UI

--- resources/views/dashboard/media-credentials/create.blade.php
+++ resources/views/dashboard/media-credentials/create.blade.php
@@ -8,29 +8,20 @@
 @endsection
 
 @section('content')
-<div class="white-header-bg">
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-8">
-                <h3 class="hidden-xs">My Media Credentials <small>&middot; Create</small></h3>
-                <h3 class="visible-xs text-center">My Media Credentials<br><small>Creae</small></h3>
-            </div>
-            <div class="col-sm-4 text-right hidden-xs">
-                <hr class="divider inv sm">
-                <a onclick="window.history.back()" class="btn btn-primary"><i class="fa fa-chevron-left icon-left"></i> Back</a>
-            </div>
-            <div class="col-sm-4 text-center visible-xs">
-                <a onclick="window.history.back()" class="btn btn-primary"><i class="fa fa-chevron-left icon-left"></i> Back</a>
-                <hr class="divider inv sm">
-            </div>
-        </div>
-    </div>
-</div>
+@breadcrumbs(['links' => [
+    'dashboard' => 'Dashboard',
+    'dashboard/records/media-credentials' => 'Travel Documents',
+    'active' => 'Media Credentials'
+]])
+@endbreadcrumbs
 <hr class="divider inv lg">
 <div class="container">
         <div class="row">
             <div class="col-xs-12">
-                <media-credential-create-update></media-credential-create-update>
+                <media-credential-create-update
+                    reservation-id="{{ request()->get('reservation') }}" 
+                    requirement-id="{{ request()->get('requirement') }}"
+                ></media-credential-create-update>
             </div>
         </div>
 </div>

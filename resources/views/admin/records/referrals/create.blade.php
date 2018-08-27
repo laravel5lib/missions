9e50e0e996779f8c5b9709226a047130b8a105d4
Neil Keena <nkeena1@gmail.com>
2018-08-27 14:54:00 -0700

    update referrals UI

--- resources/views/admin/records/referrals/create.blade.php
+++ resources/views/admin/records/referrals/create.blade.php
@@ -1,4 +1,4 @@
-@extends('admin.layouts.default')
+@extends('layouts.admin')
 
 @section('styles')
     <link rel="stylesheet" href="/css/slim.css" type="text/css">
@@ -8,31 +8,26 @@
 @endsection
 
 @section('content')
-    <div class="white-header-bg">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-8">
-                    <h3 class="hidden-xs">User Referrals <small>&middot; Create</small></h3>
-                    <h3 class="visible-xs text-center">User Referrals<br><small>Create</small></h3>
-                </div>
-                <div class="col-sm-4 text-right hidden-xs">
-                    <hr class="divider inv sm">
-                    <a onclick="window.history.back()" class="btn btn-primary"><i class="fa fa-chevron-left icon-left"></i> Back</a>
-                </div>
-                <div class="col-sm-4 text-center visible-xs">
-                    <a onclick="window.history.back()" class="btn btn-primary"><i class="fa fa-chevron-left icon-left"></i> Back</a>
-                    <hr class="divider inv sm">
-                </div>
-            </div>
-        </div>
-    </div>
+    @breadcrumbs(['links' => [
+        'admin' => 'Dashboard',
+        'admin/records/referrals' => 'Travel Documents',
+        'active' => 'Referrals'
+    ]])
+    @endbreadcrumbs
     <hr class="divider inv lg">
     <div class="container">
         <div class="row">
             <div class="col-sm-8 col-sm-offset-2">
                 <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h5>Referral</h5>
+                    </div>
                     <div class="panel-body">
-                        <referral-create-update :for-admin="true"></referral-create-update>
+                        <referral-create-update 
+                            :for-admin="true"
+                            reservation-id="{{ request()->get('reservation') }}" 
+                            requirement-id="{{ request()->get('requirement') }}"
+                        ></referral-create-update>
                     </div><!-- end panel-body -->
                 </div><!-- end panel -->
             </div>

38e9744e21bb443d3eb786c0aa5fe5036c11fb29
Neil Keena <nkeena1@gmail.com>
2018-08-28 14:07:31 -0700

    remove unused query scope

--- app/Models/v1/Questionnaire.php
+++ app/Models/v1/Questionnaire.php
@@ -12,24 +12,4 @@ class Questionnaire extends Model
     protected $fillable = ['type', 'content', 'reservation_id'];
 
     protected $casts = ['content' => 'array'];
-
-    /** 
-     * Scope a query to those managed by the given group manager.
-     * 
-     * @param  Illuminate\Database\Eloquent\Builder $query
-     * @param  String $managerId
-     * @return Illuminate\Database\Eloquent\Builder
-     */
-    public function scopeManagedBy($query, $managerId)
-    {
-        return $user->whereHas('reservations', function ($reservation) use($managerId) {
-            return $reservation->whereHas('trip', function ($trip) use($managerId) {
-                return $trip->whereHas('group', function ($group) use($managerId) {
-                    return $group->whereHas('managers', function ($manager) use($managerId) {
-                        return $manager->whereId($managerId);
-                    });
-                });
-            });
-        });
-    }
 }

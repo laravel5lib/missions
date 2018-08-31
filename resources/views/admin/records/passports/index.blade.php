bc21da6b3c4039bcaf7fc151bc5adb222513e937
Neil Keena <nkeena1@gmail.com>
2018-08-31 14:30:58 -0700

    remove header and add breadcrumb navigation

--- resources/views/admin/records/passports/index.blade.php
+++ resources/views/admin/records/passports/index.blade.php
@@ -1,26 +1,12 @@
 @extends('admin.records.index')
 
 @section('header')
-    <div class="white-header-bg">
-        <div class="container">
-            <div class="row">
-                <div class="col-xs-12 col-sm-8">
-                    <h3 class="hidden-xs">User Passports</h3>
-                    <h3 class="text-center visible-xs">User Passports</h3>
-                </div>
-                @can('create', \App\Models\v1\Passport::class)
-                <div class="col-sm-4 text-right hidden-xs">
-                    <hr class="divider inv sm">
-                    <a href="{{ url('admin/records/passports/create') }}" class="btn btn-primary"><i class="fa fa-plus icon-left"></i> Add Passport</a>
-                </div>
-                <div class="col-xs-12 text-center visible-xs">
-                    <a href="{{ url('admin/records/passports/create') }}" class="btn btn-primary"><i class="fa fa-plus icon-left"></i> Add Passport</a>
-                    <hr class="divider inv sm">
-                </div>
-                @endcan
-            </div>
-        </div>
-    </div>
+    @breadcrumbs(['links' => [
+        'admin' => 'Dashboard',
+        '/admin/records/' => 'Records',
+        'active' => ucwords(request()->segment(3))
+    ]])
+    @endbreadcrumbs
 @stop
 
 @section('tab')
